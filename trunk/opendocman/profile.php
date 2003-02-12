5e645b2f871f7313178f703f8149fe74cb88d9cd
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-02-12 18:12:52 +0000

    globalized vars
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@80 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/profile.php
+++ trunk/opendocman/profile.php
@@ -1,21 +1,21 @@
 <?PHP
 session_start();
-if (!session_is_registered('SESSION_UID'))
+if (!session_is_registered('uid'))
 {
 	header('Location:error.php?ec=1');
 	exit;
 }
 include('config.php');
 draw_header('Personal Profile');
-draw_menu($SESSION_UID);
-draw_status_bar('User Information', $last_message);
+draw_menu($_SESSION['uid']);
+@draw_status_bar('User Information', $_REQUEST['last_message']);
 ?>
 
 <html>
 <br><br>
-<?php echo '<INPUT type="hidden" name="callee" value="' . $_SERVER['PHP_SELF'] . '">'; ?>
+<INPUT type="hidden" name="callee" value="<?php echo $_SERVER['PHP_SELF']; ?>">
 <table name="list" align="center", border="1">
-<tr><td><center><a href="user.php?submit=Modify+User&item=<?php echo $SESSION_UID; ?>&callee=<?php echo $_SERVER['PHP_SELF']; ?>">Change Personal Info</a><center></td></tr>
+<tr><td><center><a href="user.php?submit=Modify+User&item=<?php echo $_SESSION['uid']; ?>&callee=<?php echo $_SERVER['PHP_SELF']; ?>">Change Personal Info</a><center></td></tr>
 </table>
 </center>
 <?php
--- trunk/opendocman/view_file.php
+++ trunk/opendocman/view_file.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-if (!session_is_registered('SESSION_UID'))
+if (!isset($_SESSION['uid']))
 {
 	header('Location:error.php?ec=1');
 	exit;
 }
 include('config.php');
-if(!isset($submit))
+if(!isset($_GET['submit']))
 {
 	draw_header('View File');
-	draw_menu($SESSION_UID);
-	draw_status_bar('File View',$last_message);
-	$file_obj = new FileData($id, $GLOBALS['connection'], $GLOBALS['database']);
+	draw_menu($_SESSION['uid']);
+	@draw_status_bar('File View',$_REQUEST['last_message']);
+	$file_obj = new FileData($_REQUEST['id'], $GLOBALS['connection'], $GLOBALS['database']);
 	$file_name = $file_obj->getName();
 	$file_id = $file_obj->getId();
 	$realname = $file_obj->getName();
@@ -25,39 +25,39 @@ if(!isset($submit))
 	//echo "suffix = $suffix<br>";
 	//echo "mime:$mimetype";	
 	echo '<form action="'.$_SERVER['PHP_SELF'].'" name="view_file_form" method="get">';
-	echo '<INPUT type="hidden" name="id" value="'.$id.'">';
+	echo '<INPUT type="hidden" name="id" value="'.$_REQUEST['id'].'">';
 	echo '<BR>';
 	// Present a link to allow for inline viewing
-	echo 'To view your file in a new window <a class="body" style="text-decoration:none" href="view_file.php?submit=view&id='.urlencode($id).'&mimetype='.urlencode("$mimetype").'">Click Here</a><br><br>';
+	echo 'To view your file in a new window <a class="body" style="text-decoration:none" href="view_file.php?submit=view&id='.urlencode($_REQUEST['id']).'&mimetype='.urlencode("$mimetype").'">Click Here</a><br><br>';
 	echo 'If you are not able to do so for some reason, ';
 	echo 'click <input type="submit" name="submit" value="Download"> to download the selected document and begin downloading it to your local workstation for local view.';
 	echo '</form>';
 
     draw_footer();
 }
-elseif ($submit == 'view')
+elseif ($_GET['submit'] == 'view')
 {
 	//echo "mimetype = $mimetype<br>";
 	//exit;
-	//echo "ID is $id";
-	$file_obj = new FileData($id, $GLOBALS['connection'], $GLOBALS['database']);
+	//echo "ID is $_REQUEST['id']";
+	$file_obj = new FileData($_REQUEST['id'], $GLOBALS['connection'], $GLOBALS['database']);
 	$realname = $file_obj->getName();
-	$filename = $GLOBALS['CONFIG']['dataDir'] . $id . ".dat";
+	$filename = $GLOBALS['CONFIG']['dataDir'] . $_REQUEST['id'] . ".dat";
 	// send headers to browser to initiate file download
 	header('Content-Length: '.filesize($filename));
 	// Pass the mimetype so the browser can open it
-        header('Content-Type: ' . $mimetype);
+        header('Content-Type: ' . $_GET['mimetype']);
         header('Content-Disposition: inline; filename=' . $realname);
         // Apache is sending Last Modified header, so we'll do it, too
         $modified=filemtime($filename);
         header('Last-Modified: '. date('D, j M Y G:i:s T',$modified));   // something like Thu, 03 Oct 2002 18:01:08 GMT
 	readfile($filename);
 }
-elseif ($submit=='Download')
+elseif ($_GET['submit'] == 'Download')
 {
-	$file_obj = new FileData($id, $GLOBALS['connection'], $GLOBALS['database']);
+	$file_obj = new FileData($_REQUEST['id'], $GLOBALS['connection'], $GLOBALS['database']);
 	$realname = $file_obj->getName();
-	$filename = $GLOBALS['CONFIG']['dataDir'] . $id . ".dat";
+	$filename = $GLOBALS['CONFIG']['dataDir'] . $_REQUEST['id'] . ".dat";
 	// send headers to browser to initiate file download
 	header('Cache-control: private');
 	header ('Content-Type: application/octet-stream');
@@ -70,6 +70,6 @@ elseif ($submit=='Download')
 else
 {
 echo 'Nothing to do ';
-echo 'submit is ' . $submit;
+echo 'submit is ' . $_GET['submit'];
 }
 ?>
