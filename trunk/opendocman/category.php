a917c5c0d599bd88c6bde368339444e25e114e1a
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-02-26 16:02:30 +0000

    globalized
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@114 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/category.php
+++ trunk/opendocman/category.php
@@ -2,7 +2,7 @@
 // category.php - Administer Categories
 // check for valid session 
 session_start();
-if (!session_is_registered('uid'))
+if (!isset($_SESSION['uid']))
 {
 	draw_error('error.php?ec=1');
 	exit;
@@ -10,19 +10,19 @@ if (!session_is_registered('uid'))
 // includes
 include('config.php');
 
-if(isset($submit) and $submit != 'Cancel')
+if(isset($_REQUEST['submit']) and $_REQUEST['submit'] != 'Cancel')
 {
 	draw_menu($_SESSION['uid']);
 }
 
-if($submit == 'add')
+if(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'add')
 {
-                if (!isset($last_message))
+                if (!isset($_REQUEST['last_message']))
                 {
-                        $last_message='';
+                        $_REQUEST['last_message']='';
                 }
 		draw_header('Add New Category');
-		draw_status_bar('Add New Category', $last_message);
+		draw_status_bar('Add New Category', $_REQUEST['last_message']);
 		// Check to see if user is admin
 		$user_obj = new User($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
 		if(!$user_obj->isAdmin())        
@@ -32,7 +32,7 @@ if($submit == 'add')
 		}
 ?>
 <center>
-<form action="commitchange.php?last_message=<?php $last_message; ?>" method="GET" enctype="multipart/form-data">
+<form action="commitchange.php?last_message=<?php $_REQUEST['last_message']; ?>" method="GET" enctype="multipart/form-data">
 <table border="0" cellspacing="5" cellpadding="5">
 	<tr>
 		<td><b>Category</b></td>
@@ -51,29 +51,33 @@ if($submit == 'add')
 <?php
 	draw_footer();
 }
-elseif($submit == 'delete')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'delete')
 {
 	$delete='';
+        if (!isset($_REQUEST['last_message']))
+        {       
+                $_REQUEST['last_message']='';
+        }
 	draw_header('Category Deletion');
-	draw_status_bar('Delete Item', $last_message);
+	draw_status_bar('Delete Item', $_REQUEST['last_message']);
 	// query to show item
 	echo '<center>'; 
 	echo '<table border=0>';
-	$query = 'SELECT id, name FROM category where id="' . $item . '"';
+	$query = 'SELECT id, name FROM category where id="' . $_REQUEST['item'] . '"';
 	$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
-	while(list($id, $name) = mysql_fetch_row($result))
+	while(list($lid, $lname) = mysql_fetch_row($result))
 	{
-		echo '<tr><td>Id # :</td><td>' . $id . '</td></tr>';
-		echo '<tr><td>Name :</td><td>' . $name . '</td></tr>';
+		echo '<tr><td>Id # :</td><td>' . $lid . '</td></tr>';
+		echo '<tr><td>Name :</td><td>' . $lname . '</td></tr>';
 	}
 ?>
 	<TABLE name="delete_table">
-	<form action="commitchange.php?last_message=<?php echo $last_message; ?>&id=<?php echo $item;?> " method="POST" enctype="multipart/form-data">
+	<form action="commitchange.php?last_message=<?php echo $_REQUEST['last_message']; ?>&id=<?php echo $_REQUEST['item'];?> " method="POST" enctype="multipart/form-data">
 		<tr>
 			<td valign="top">Are you sure you want to delete this?</td>
 			<td colspan="4" align="center"><input type="Submit" name="deletecategory" value="Yes"></td>
 	</form>
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $last_message; ?>" method="POST" enctype="multipart/form-data">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $_REQUEST['last_message']; ?>" method="POST" enctype="multipart/form-data">
 		<td colspan="4" align="center"><input type="Submit" name="" value="No, Cancel"></td>
 	</form>
 		</tr>
@@ -81,15 +85,15 @@ elseif($submit == 'delete')
 <?php
 	draw_footer();
 }
-elseif($submit == 'deletepick')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'deletepick')
 {
-        if (!isset($last_message))
+        if (!isset($_REQUEST['last_message']))
         {
-                $last_message='';
+                $_REQUEST['last_message']='';
         }
 	$deletepick='';
 	draw_header('Category Selection');
-	draw_status_bar('Choose Item to Delete', $last_message);
+	draw_status_bar('Choose Item to Delete', $_REQUEST['last_message']);
 ?>
 	<center>
 	<table border="0" cellspacing="5" cellpadding="5">
@@ -100,10 +104,10 @@ elseif($submit == 'deletepick')
 <?php
 	$query = 'SELECT id, name FROM category ORDER BY name';
 	$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
-	while(list($id, $name) = mysql_fetch_row($result))
+	while(list($lid, $lname) = mysql_fetch_row($result))
 	{
-		$str = '<option value="' . $id . '"';
-		$str .= '>' . $name . '</option>';
+		$str = '<option value="' . $lid . '"';
+		$str .= '>' . $lname . '</option>';
 		echo $str;
 	}
 	mysql_free_result ($result);
@@ -123,24 +127,28 @@ elseif($submit == 'deletepick')
 <?php
 	draw_footer();
 }
-elseif($submit == 'Show Category')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'Show Category')
 {
 	// query to show item
 	draw_header('Category Information');
-	draw_status_bar('Display Item Information', $last_message);
+        if (!isset($_REQUEST['last_message']))
+        {
+                $_REQUEST['last_message'] = '';
+        }
+	draw_status_bar('Display Item Information', $_REQUEST['last_message']);
 	echo '<center>';
 	// Select name
-	$query = "SELECT category.name FROM category where category.id='$item'";
+	$query = "SELECT category.name FROM category where category.id='$_REQUEST[item]'";
 	$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
 	echo('<table name="main" cellspacing="15" border="0">');
-	list($category) = mysql_fetch_row($result);
+	list($lcategory) = mysql_fetch_row($result);
 	echo '<th>Name</th><th>ID</th>';
 	echo '<tr>';
-	echo '<td>'.$category.'</td>';	
-	echo '<td>'.$item.'</td>';
+	echo '<td>' . $lcategory . '</td>';	
+	echo '<td>' . $_REQUEST['item'] . '</td>';
 	echo '</tr>';	
 ?>
-	<form action="admin.php?last_message=<?php echo $last_message; ?>" method="POST" enctype="multipart/form-data">
+	<form action="admin.php?last_message=<?php echo $_REQUEST['last_message']; ?>" method="POST" enctype="multipart/form-data">
 		<tr>
 			<td colspan="4" align="center"><input type="Submit" name="" value="Back"></td>
 		</tr>
@@ -150,28 +158,28 @@ elseif($submit == 'Show Category')
 	
 	draw_footer();
 }
-elseif($submit == 'showpick')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'showpick')
 {
-                if (!isset($last_message))
+                if (!isset($_REQUEST['last_message']))
                 {
-                        $last_message='';
+                        $_REQUEST['last_message']='';
                 }       
 		draw_header('Category Selection');
-		draw_status_bar('Choose item to view', $last_message);
+		draw_status_bar('Choose item to view', $_REQUEST['last_message']);
 		$showpick='';
 ?>
 			<center>
 			<table border="0" cellspacing="5" cellpadding="5">
-			<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $last_message; ?>" method="POST" enctype="multipart/form-data">
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $_REQUEST['last_message']; ?>" method="POST" enctype="multipart/form-data">
 			<tr>
 			<td><b>Category</b></td>
 			<td colspan="3"><select name="item">
 <?php
 			$query = 'SELECT id, name FROM category ORDER BY name';
 			$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
-		while(list($id, $name) = mysql_fetch_row($result))
+		while(list($lid, $lname) = mysql_fetch_row($result))
 		{
-				echo '<option value="' . $id . '">' . $name . '</option>';
+				echo '<option value="' . $lid . '">' . $lname . '</option>';
 		}
 		mysql_free_result ($result);
 ?>
@@ -191,28 +199,28 @@ elseif($submit == 'showpick')
 <?php
 	draw_footer();
 }
-elseif($submit == 'Update')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'Update')
 {
-        if (!isset($last_message))
+        if (!isset($_REQUEST['last_message']))
         {
-                $last_message='';
+                $_REQUEST['last_message']='';
         }
 	draw_header('Update Category');
-	draw_status_bar('Update Item', $last_message);
+	draw_status_bar('Update Item', $_REQUEST['last_message']);
 ?>
 	<center>
 		<table border="0" cellspacing="5" cellpadding="5">
 			<tr>
-		<form action="commitchange.php?last_message=<?php echo $last_message; ?>" method="POST" enctype="multipart/form-data">
+		<form action="commitchange.php?last_message=<?php echo $_REQUEST['last_message']; ?>" method="POST" enctype="multipart/form-data">
 <?php
 	// query to get a list of users
-	$query = "SELECT id, name FROM category where id='$item'";
+	$query = "SELECT id, name FROM category where id='$_REQUEST[item]'";
 	$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
-	while(list($id, $name) = mysql_fetch_row($result))
+	while(list($lid, $lname) = mysql_fetch_row($result))
 	{
 		echo '<tr>';
-		echo '<td><input type="textbox" name="name" value="'.$name.'"></td>';
-		echo '<td><input type="hidden" name="id" value="'.$id.'"></td>';
+		echo '<td><input type="textbox" name="name" value="' . $lname . '"></td>';
+		echo '<td><input type="hidden" name="id" value="' . $lid . '"></td>';
 		echo '</tr>';
 	}
 	mysql_free_result ($result);
@@ -220,7 +228,7 @@ elseif($submit == 'Update')
 		<td>
 			<input type="Submit" name="updatecategory" value="Modify Category">
 	</form>
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $last_message; ?>">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>?last_message=<?php echo $_REQUEST['last_message']; ?>">
 		<input type="Submit" name="submit" value="Cancel">
 	</form>
 			</td>
@@ -230,14 +238,14 @@ elseif($submit == 'Update')
 <?php
 draw_footer();
 }
-elseif($submit == 'updatepick')
+elseif(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'updatepick')
 {
-        if (!isset($last_message))
+        if (!isset($_REQUEST['last_message']))
         {
-                $last_message='';
+                $_REQUEST['last_message']='';
         }
 	draw_header('Category Selection');
-	draw_status_bar('Modify Item',$last_message);
+	draw_status_bar('Modify Item',$_REQUEST['last_message']);
 ?>
 	<center>
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
@@ -249,9 +257,9 @@ elseif($submit == 'updatepick')
 	// query to get a list of users
 	$query = "SELECT id, name FROM category ORDER BY name";
 	$result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
-	while(list($id, $name) = mysql_fetch_row($result))
+	while(list($lid, $lname) = mysql_fetch_row($result))
 	{
-		echo '<option value="'.$id.'">'.$name.'</option>';
+		echo '<option value="' . $lid . '">' . $lname . '</option>';
 	}
 	mysql_free_result ($result);
 ?>
@@ -271,9 +279,9 @@ elseif($submit == 'updatepick')
 <?php
 	draw_footer();
 }
-elseif ($submit == 'Cancel')
+elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'Cancel')
 {
-		$last_message=urlencode('Action canceled');
-		header ('Location: admin.php?last_message='.$last_message);
+		$_REQUEST['last_message']=urlencode('Action canceled');
+		header ('Location: admin.php?last_message=' . $_REQUEST['last_message']);
 }
 ?>
