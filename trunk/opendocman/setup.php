22e8e2e3b0c4d5eaa33cfae0137367f7adf4e8b1
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-03-05 21:15:27 +0000

    initial import of installation script
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@135 769e0422-6c0b-0410-966c-d94082ee0ac6

---
+++ trunk/opendocman/setup.php
@@ -0,0 +1,112 @@
+<?php include("config.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Configurator</title>
+</head>
+
+<body>
+<center>
+<img src="images/logo.gif"><br>
+<?php
+
+switch(@$_REQUEST['op']) {
+    
+    case "install":
+         get_info();
+         break;
+
+    case "commitinstall":
+         do_install();
+         break;
+
+    case "update_10":
+         do_update_10();
+         break;
+    
+    default:
+         print_intro();
+         break;
+}
+
+
+
+
+function get_info()
+{
+?>
+Please complete the following form to create your new database
+<form name="newinstall" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+ <table align="center">
+  <tr>
+   <td>
+    <input type="text" name="rootname"> Mysql Root User <br>
+    <input type="password" name="rootpass"> Mysql Root Password <br>
+    <input type="submit" name="op" value="commitinstall"><br>
+   </td>
+  </tr>
+ </table>
+</form>
+    
+<?php
+}
+
+function do_install()
+{
+        echo 'installing...<br>';
+
+
+        mysql_connect($GLOBALS['hostname'], $_REQUEST['rootname'], $_REQUEST['rootpass']) or die ("Unable to connect!");
+
+        // Create database
+        $result = mysql_query("
+        CREATE DATABASE $GLOBALS[database];
+        ") or die("<br>Unable to Create Database - Error in query:" . mysql_error());
+
+        mysql_select_db($GLOBALS['database']) or die (mysql_error() . "<br><font class=\"pn-failed\">Unable to select database.</font>");
+
+        // Grant privs
+        $result = mysql_query("
+        GRANT ALL ON $GLOBALS[database].* to $GLOBALS[user] identified by '$GLOBALS[pass]';
+        ") or die("<br>Could not set GRANT");
+
+        include("install/odm1.1rc1.php");
+
+        echo 'All Done with installation! Click <a href="' . $GLOBALS['CONFIG']['base_url'] . '">HERE</a> to login';
+} // End Install
+
+
+function do_update_10()
+{
+        echo 'Updating';        
+        mysql_connect($GLOBALS['hostname'], $GLOBALS['user'], $GLOBALS['pass']) or die ("Unable to connect!"); 
+        mysql_select_db($GLOBALS['database']) or die (mysql_error() . "<br><font class=\"pn-failed\">Unable to select database.</font>");
+        include("install/upgrade_10.php");
+        echo 'All Done with update! Click <a href="' . $GLOBALS['CONFIG']['base_url'] . '">HERE</a> to login';
+}
+
+function print_intro()
+{
+?>
+<h3>Welcome to the OpenDocMan Configuration Tool</h3>
+</center>
+<table align="center">
+ <tr>
+  <td>Your current version is <?php echo $GLOBALS['CONFIG']['current_version']; ?>:<br><br></td>
+ </tr>
+ <tr>
+  <td>Please choose one from the following:<br><br></td>
+ </tr>
+ <tr>
+  <td><a href="setup.php?op=install">New Installation</a><br><br></td>
+ </tr>
+ <tr>
+  <td><a href="setup.php?op=update_10">Upgrade from version 1.0</a><br><br></td>
+ </tr>
+</table>
+<?php
+}
+
+?>
+</body>
+</html>
