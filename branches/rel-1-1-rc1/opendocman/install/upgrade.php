c7e53f9ae2a88ec50af456aa2e48f089173a8729
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-03-25 23:34:35 +0000

    fixed typo added 11rc1
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@175 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-1-rc1/opendocman/install/upgrade.php
+++ branches/rel-1-1-rc1/opendocman/install/upgrade.php
@@ -4,6 +4,14 @@ function do_upgrade10 ($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype) {
     global $dbconn;
     mysql_connect($dbhost, $dbuname, $dbpass);
     mysql_select_db("$dbname") or die ("<br><font class=\"pn-failed\">Unable to select database.</font>");
-    include("install/upgrade_1.0.php");
+    include("install/upgrade_10.php");
 }
+/*** This function calls the upgrade from odm 1.1rc1 ***/
+function do_upgrade11rc1 ($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype) {
+    global $dbconn;
+    mysql_connect($dbhost, $dbuname, $dbpass);
+    mysql_select_db("$dbname") or die ("<br><font class=\"pn-failed\">Unable to select database.</font>");
+    include("install/upgrade_11rc1.php");
+}
+
 ?>
