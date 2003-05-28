42d6d36e14a9a12bc6f435c2aa92ed5fdaff58e8
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-05-28 18:33:02 +0000

    added 1.1 upgrade
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@251 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/install/upgrade.php
+++ trunk/opendocman/install/upgrade.php
@@ -21,5 +21,13 @@ function do_upgrade11rc2 ($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype)
     include("install/upgrade_11rc2.php");
 }
 
+/*** This function calls the upgrade from odm 1.1 ***/
+function do_upgrade11 ($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype) {
+    global $dbconn;
+    mysql_connect($dbhost, $dbuname, $dbpass);
+    mysql_select_db("$dbname") or die ("<br><font class=\"pn-failed\">Unable to select database.</font>");
+    include("install/upgrade_11.php");
+}
+
 
 ?>
