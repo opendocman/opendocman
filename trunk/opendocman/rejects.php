7c21be3f261814d3169c3839b9411ca966084651
khoang82 <khoang82@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-05-28 23:40:01 +0000

    1.2-rc1 code merge
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@257 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/rejects.php
+++ trunk/opendocman/rejects.php
@@ -2,34 +2,34 @@
 session_start();
 if (!session_is_registered('uid'))
 {
-        header('Location:error.php?ec=1');
-        exit;
+        header('Location:index.php?redirection=' . urlencode( $_SERVER['REQUEST_URI']) );
+		exit;
 }
 include ('./config.php');
 // includes
-if(!isset($_POST['starting_index']))
+if(!isset($_REQUEST['starting_index']))
 {
-        $_POST['starting_index'] = 0;
+        $_REQUEST['starting_index'] = 0;
 }
 
-if(!isset($_POST['stoping_index']))
+if(!isset($_REQUEST['stoping_index']))
 {
-        $_POST['stoping_index'] = $_POST['starting_index']+$GLOBALS['CONFIG']['page_limit']-1;
+        $_REQUEST['stoping_index'] = $_REQUEST['starting_index']+$GLOBALS['CONFIG']['page_limit']-1;
 }
 
-if(!isset($_POST['sort_by']))
+if(!isset($_REQUEST['sort_by']))
 {
-        $_POST['sort_by'] = 'id';
+        $_REQUEST['sort_by'] = 'id';
 }
 
-if(!isset($_POST['sort_order']))
+if(!isset($_REQUEST['sort_order']))
 {
-        $_POST['sort_order'] = 'a-z';
+        $_REQUEST['sort_order'] = 'asc';
 }
 
-if(!isset($_POST['page']))
+if(!isset($_REQUEST['page']))
 {
-        $_POST['page'] = 0;
+        $_REQUEST['page'] = 0;
 }
 
 $with_caption = false;
@@ -38,25 +38,29 @@ if(!isset($_POST['submit']))
 {
         draw_menu($_SESSION['uid']);
         draw_header('Rejected Files');
-        @draw_status_bar('Rejected Document Listing', $_POST['last_message']);
-        $page_url = $_SERVER['PHP_SELF'] . '?';
+        @draw_status_bar('Rejected Document Listing', $_REQUEST['last_message']);
+        $page_url = $_SERVER['PHP_SELF'] . '?mode=' . @$_REQUEST['mode'];
 
         $user_obj = new User($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
         $userperms = new UserPermission($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
-        $fileobj_array = $user_obj->getRejectedFiles();
-        $sorted_obj_array = obj_array_sort_interface($fileobj_array, $_POST['sort_order'], $_POST['sort_by']);
-        echo '<FORM name="table" method="POST" action="' . $_SERVER['PHP_SELF'] . '">' . "\n";
-
-?>	
+		if($user_obj->isRoot() && @$_REQUEST['mode'] == 'root')
+			$fileid_array = $user_obj->getAllRejectedFileIds();
+		else
+			$fileid_array = $user_obj->getRejectedFileIds();
+        $sorted_id_array = my_sort($fileid_array, $_REQUEST['sort_order'], $_REQUEST['sort_by']);
+		?>
+		 <?php
+			 if(@$_REQUEST['mode']=='root')
+				 echo '<FORM name="author_note_form" action="' . $_SERVER['PHP_SELF'] . '?mode=root"' . ' onsubmit="closeWindow(1250);" method="POST">';
+			 else
+				 echo '<FORM name="author_note_form" action="' . $_SERVER['PHP_SELF'] . '" onsubmit="closeWindow(1000);" method="POST">';?>
                 <TABLE border="1"><TR><TD>
 
 <?php
 
-                list_files($sorted_obj_array, $userperms, $page_url, $GLOBALS['CONFIG']['dataDir'], $_POST['sort_order'],  $_POST['sort_by'], $_POST['starting_index'], $_POST['stoping_index'], true, $with_caption);
-        list_nav_generator(sizeof($sorted_obj_array), $GLOBALS['CONFIG']['page_limit'], $page_url, $_POST['page'], $_POST['sort_by'], $_POST['sort_order']);
-
+        list_files($sorted_id_array, $userperms, $page_url, $GLOBALS['CONFIG']['dataDir'], $_REQUEST['sort_order'],  $_REQUEST['sort_by'], $_REQUEST['starting_index'], $_REQUEST['stoping_index'], true, $with_caption);
+        list_nav_generator(sizeof($sorted_id_array), $GLOBALS['CONFIG']['page_limit'], $GLOBALS['CONFIG']['num_page_limit'], $page_url, $_REQUEST['page'], $_REQUEST['sort_by'], $_REQUEST['sort_order']);
 ?>
-
                 </TD></TR><TR><TD><CENTER><INPUT type="SUBMIT" name="submit" value="Re-Submit For Review"><INPUT type="submit" name="submit" value="Delete file(s)">
                 </TABLE></FORM>
 
@@ -75,11 +79,11 @@ elseif($_POST['submit'] == 'Re-Submit For Review')
                         //mail($mail_to, $mail_subject. $file_obj->getName(), ($mail_greeting.$file_obj->getName().' '.$mail_body.$mail_salute), $mail_headers);
                         $file_obj->Publishable(0);
                 }
-        header('Location:' . $_SERVER['PHP_SELF'] . '?last_message=File authorization completed successfully');
+        header('Location:' . $_SERVER['PHP_SELF'] . '?mode=' . @$_REQUEST['mode'] . '&last_message=File authorization completed successfully');
 }
 elseif($_POST['submit']=='Delete file(s)')
 {
-        $url = 'delete.php?';
+        $url = 'delete.php?mode=tmpdel&';
         $id = 0;
         for($i = 0; $i<$_POST['num_checkboxes']; $i++)
                 if(isset($_POST["checkbox$i"]))
--- trunk/opendocman/search.php
+++ trunk/opendocman/search.php
@@ -1,18 +1,28 @@
 <?php
-/*$where='all';
+/*
+$where='all';
   $keyword='Nguyen Khoa';
   $_SESSION['uid']=102;
   $submit='submit';
 */
-session_start();
+
+/*session_start();
 if (!session_is_registered('uid'))
 {
-        header('Location:error.php?ec=1');
-        exit;
+        header('Location:index.php?redirection=' . urlencode( $_SERVER['REQUEST_URI']) );
+		exit;
 }
-/// includes
-include('config.php');
+*/
+$_GET['submit']='';
+$_SESSION['uid']=102;
+$_GET['keyword']='-1';
+$_GET['where']='author_locked_files';
+$_GET['exact_phrase']='on';
+$_GET['case_sensitivity']='';
 
+/// includes
+$start_time = time();
+include('config.php');
 draw_header('Search');
 draw_menu($_SESSION['uid']);
 draw_status_bar('Search', "");
@@ -31,14 +41,13 @@ if(!isset($_GET['sort_by']))
 }
 if(!isset($_GET['sort_order']))
 {
-        $_GET['sort_order'] = 'a-z';
+        $_GET['sort_order'] = 'asc';
 }
 if(!isset($_GET['page']))
 {
         $_GET['page'] = 0;
 }
 echo '<body bgcolor="white">';
-
 if(!isset($_GET['submit']))
 {
         ?>
@@ -51,13 +60,13 @@ if(!isset($_GET['submit']))
                    <tr>
                     <td valign="top"><b>Search term</b></td>
                     <td><input type="Text" name="keyword" size="50"></td>
-                    <td>Exact Word: <input type="checkbox" name="exact_word"></td>
+                    <td>Exact Phrase: <input type="checkbox" name="exact_phrase"></td>
                     <td>Case Sensitivity <input type="checkbox" name="case_sensitivity"></td>
                    </tr>
                    <tr>
                     <td valign="top"><b>Search</b></td>
                     <td><select name="where">
-                      <option value="author_only">Author only</option>
+                      <option value="author_only">Author (Last_name  First_name)</option>
                       <option value="department_only">Department only</option>
                       <option value="category_only">Category only</option>
                       <option value="descriptions_only">Descriptions only</option>
@@ -79,164 +88,127 @@ if(!isset($_GET['submit']))
             </center>
 
 <?php
-
+echo '<br><b>Load Time: ' . time() - $start_time;
 draw_footer();
 
 }
 else
 {
-        sort_browser();
-        function OBJs_search_interface($where, $query, $exact_word, $case_sensitivity, $OBJ_array)
+    function search($lwhere, $lkeyword, $lexact_phrase, $lcase_sensitivity, $lsearch_array)    
+    {
+    	$lkeyword2 = $lkeyword;
+		$l_remain = strstr($lkeyword, ' ');
+		if($l_remain!='')
+			$lkeyword = strtok($lkeyword, ' ');
+		$lequate = '=';
+    	if( $lexact_phrase!='on' )
+    	{	
+    		$lkeyword = '%' . $lkeyword . '%';
+    		
+    	}
+    	if($lcase_sensitivity!='on')
+    	{		
+    		$lequate = ' LIKE ';
+    	}
+    	else 
+    	{
+    		$lequate = ' REGEXP BINARY';
+    	}
+    	
+    	$lquery = 'SELECT data.id FROM data, user, department, category WHERE data.owner = user.id AND data.department=department.id AND data.category = category.id AND (';
+    	$larray_len = sizeof($lsearch_array);
+    	switch($lwhere)
         {
-                if($where == 'all')
-                {
-                        $cases = array('filenames_only', 'descriptions_only', 'comments_only', 'author_only', 'department_only', 'category_only');
-                        $cases_len = sizeof($cases);
-                        for($i = 0; $i<$cases_len; $i++)
-                        {
-                                $query_array = OBJs_to_strs($cases[$i],  $query, $exact_word, $case_sensitivity, $OBJ_array);
-                                $search_result = str_search($query, $query_array, $exact_word, $case_sensitivity);
-                                for($j = 0; $j < sizeof($search_result); $j++)
-                                        $search_result[$j] = array($search_result[$j], $cases[$i]);
-                                if($i == 0)
-                                {
-                                        $result_array = $search_result;
-                                }
-                                else
-                                {
-                                        $result_array = merge2DArrays($result_array, $search_result, true, 0);
-                                }	
-                        }
-                        $search_result = $result_array;
-                }
-                else
-                {
-                        $query_array = OBJs_to_strs($where, $query, $exact_word, $case_sensitivity, $OBJ_array);
-                        $search_result = str_search($query, $query_array, $exact_word, $case_sensitivity);
-                        for($j = 0; $j < sizeof($search_result); $j++)
-                        {
-                        	$search_result[$j] = array($search_result[$j], $where);
-                        }
-                }	
-                $search_result_len = sizeof($search_result);
-                $sorted_result = array();
-                $sorted_result_index = 0;
-                for($i = 0; $i<$search_result_len; $i++)
-                {
-                        if($search_result[$i][0] == true)
-                        {
-                                $sorted_result[$sorted_result_index++] = $OBJ_array[$i];
-                        }
-                }
-                return $sorted_result;
-        // End function OBJs_search_interface
+        	// Put all the category for each of the OBJ in the OBJ array into an array
+            // Notice, the index of the OBJ_array and the category array are synchronized.
+            case 'author_locked_files':
+                $lquery .= 'data.status' . $lequate  . '\'' . $lkeyword . '\' AND data.owner=\'' . $_SESSION['uid'] . '\'';
+                break;
+        	
+        	// Put all the category for each of the OBJ in the OBJ array into an array
+            // Notice, the index of the OBJ_array and the category array are synchronized.
+            case 'category_only':
+                $lquery .= 'category.name' . $lequate  . '\'' . $lkeyword . '\'';
+                break;
+                // Put all the author name for each of the OBJ in the OBJ array into an array
+                // Notice, the index of the OBJ_array and the author name array are synchronized.
+            case 'author_only':
+        if( $lexact_phrase=='on' )
+		{ $lquery .= 'user.first_name' . $lequate . '\'' . substr($l_remain, 1) . '\' AND ' . 'user.last_name' . $lequate . '\'' . $lkeyword . '\'';
+
+		}
+		else
+		{
+				$lquery .= 'user.first_name' . $lequate  . '\'' . $lkeyword . '\' OR ' . 'user.last_name' . $lequate . '\'' . $lkeyword . '\'';
+		}
+				break;
+                // Put all the department name for each of the OBJ in the OBJ array into an array
+                // Notice, the index of the OBJ_array and the department name array are synchronized.case 'department_only':
+            case 'department_only':
+                $lquery .= 'department.name' . $lequate  . '\'' . $lkeyword . '\'';
+                break;
+                // Put all the description for each of the OBJ in the OBJ array into an array
+                // Notice, the index of the OBJ_array and the description array are synchronized.
+            case 'descriptions_only':
+                $lquery .= 'data.description' . $lequate  . '\'' . $lkeyword . '\'';
+                break;
+                // Put all the file name for each of the OBJ in the OBJ array into an array
+                // Notice, the index of the OBJ_array and the file name array are synchronized.
+            case 'filenames_only':
+                $lquery .= 'data.realname= \'' . $lkeyword . '\'';
+                break;
+                // Put all the comments for each of the OBJ in the OBJ array into an array
+                // Notice, the index of the OBJ_array and the comments array are synchronized.
+            case 'comments_only':
+                $lquery .= 'data.comment' . $lequate  . '\'' . $lkeyword . '\'';
+                break;
+            case 'all':
+            	$lquery .= 'category.name' . $lequate  . '\'' . $lkeyword . '\' OR ' . 
+            				'user.first_name' . $lequate  . '\'' . $lkeyword . '\' OR ' . 'user.last_name ' . $lequate  . '\'' . $lkeyword . '\' OR ' . 
+            				'department.name' . $lequate  . '\'' . $lkeyword . '\' OR ' . 
+            				'data.description' . $lequate  . '\'' . $lkeyword . '\' OR ' . 
+            				'data.realname' . $lequate  . '\'' . $lkeyword . '\' OR ' . 
+            				'data.comment' . $lequate  . '\'' . $lkeyword . '\'';
+            	break;
+            default : break;
         }
-
-/*
-           OBJ_array_search search for the string query in the where field of every data obj.
-Ex: $where='category_only', $query='test', $exact_word=true, $case_sensitivity=true, $OBJ_array
-OBJ_array_search would put all the categories of the all the element in the OBJ_array into an array;
-then, OBJ_array_search would submit this string into the interface along with $where, $query, and ...
-
-Ex: $where='all', ..., OBJ_array
-OBJ_array_search will called itself to do a search in every supported fields.
-
-Return: OBJ_array_search will return a 2D array. return_array[index1][index2] where
-index1 points to data for an object and index2 points to the OBJ it self or the field that 
-the OBJ statisfied.  Let's OBJ1 was found in the by doing a search on filename_only
-then return_array[index1] = (OBJ1, 'filename_only'); 
-         */
-        function OBJs_to_strs($where, $query, $exact_word, $case_sensitivity, $OBJ_array)
-        {
-                if( sizeof( $OBJ_array ) == 0 )
-					return $OBJ_array;
-				$obj_array_len = sizeof($OBJ_array);
-                //all search cases supported by this search engine.
-                //use to perform an all search.
-                $cases = array('filenames_only', 'descriptions_only', 'comments_only', 'author_only', 'department_only', 'category_only');
-                $user_obj = new User(1, $OBJ_array[0]->connection, $OBJ_array[0]->database);
-                $dept_obj = new Department(1, $OBJ_array[0]->connection, $OBJ_array[0]->database);
-                switch($where)
-                {
-                        // Put all the category for each of the OBJ in the OBJ array into an array
-                        // Notice, the index of the OBJ_array and the category array are synchronized.
-                        case 'category_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                        $query_array[$i] = $OBJ_array[$i]->getCategoryName();
-                                }
-                                break;
-                                // Put all the author name for each of the OBJ in the OBJ array into an array
-                                // Notice, the index of the OBJ_array and the author name array are synchronized.
-                        case 'author_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                    $full_name_array = $OBJ_array[$i]->getOwnerFullName();    
-                                	$query_array[$i] = $full_name_array[1] . ' ' . $full_name_array[0];
-                                }
-                                break;
-
-                                // Put all the department name for each of the OBJ in the OBJ array into an array
-                                // Notice, the index of the OBJ_array and the department name array are synchronized.case 'department_only':
-                        case 'department_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                        $dept_obj->setId($OBJ_array[$i]->getDepartment());
-                                        $query_array[$i] = $dept_obj->getName();
-                                }
-                                break;
-                                // Put all the description for each of the OBJ in the OBJ array into an array
-                                // Notice, the index of the OBJ_array and the description array are synchronized.
-                        case 'descriptions_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                        $query_array[$i] = $OBJ_array[$i]->getDescription();
-                                }
-                                break;
-                                // Put all the file name for each of the OBJ in the OBJ array into an array
-                                // Notice, the index of the OBJ_array and the file name array are synchronized.
-                        case 'filenames_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                        $query_array[$i] = $OBJ_array[$i]->getName();
-                                }
-                                break;
-                                // Put all the comments for each of the OBJ in the OBJ array into an array
-                                // Notice, the index of the OBJ_array and the comments array are synchronized.
-                        case 'comments_only':
-                                for($i = 0; $i<$obj_array_len; $i++)
-                                {
-                                        $query_array[$i] = $OBJ_array[$i]->getComment();
-                                }
-                                break;
-                        default : break;
-                }
-                return $query_array;
-        }// End function OBJs_to_strs
-
-        $current_user = new User($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
-        $user_perms = new User_Perms($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
-        $current_user_permission = new UserPermission($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
-        $view_able_files_obj = $current_user_permission->getAllowedFileOBJs();
-        $obj_array_len = sizeof($view_able_files_obj);
-        $query_array = array();
-        $search_result = OBJs_search_interface($_GET['where'], $_GET['keyword'], @$_GET['exact_word'], @$_GET['case_sensitivity'], $view_able_files_obj);
-/*
-      if($where == 'all')
-          {
-          $array_len = sizeof($search_result);
-          for($i = 0; $i<$array_len; $i++)
-          {
-          $search_result[$i] = $search_result[$i][0];
-          }
-          }
-*/
-        $page_url = $_SERVER['PHP_SELF'].'?keyword='.$_GET['keyword'].'&where='.$_GET['where'].'&submit='.$_GET['submit'];
-        $sorted_obj_array = obj_array_sort_interface($search_result, $_GET['sort_order'], $_GET['sort_by']);
-        list_files($sorted_obj_array,  $current_user_permission, $page_url,  $GLOBALS['CONFIG']['dataDir'], $_GET['sort_order'], $_GET['sort_by'], $_GET['starting_index'], $_GET['stoping_index']);
-        echo '<BR>';
-        list_nav_generator(sizeof($sorted_obj_array), $GLOBALS['CONFIG']['page_limit'], $page_url,$_GET['page'], $_GET['sort_by'], $_GET['sort_order'] );
-
-        draw_footer();
+  	 	$lquery .= ') ORDER BY data.id ASC';
+		//echo $lquery;
+  	 	$lresult = mysql_query($lquery) or die("Error in query: $lquery" . mysql_error() );
+  	 	$lindex = 0;//echo '----' . $lquery;
+  	 	$lid_array = array();
+  	 	$llen = mysql_num_rows($lresult);
+  	 	while( $lindex < $llen )
+  	 	{ 	list($lid_array[$lindex++]) = mysql_fetch_row($lresult);	} 
+		if($l_remain != '' && $lexact_phrase != "on")
+		{
+			 return array_values( array_intersect($lid_array, ( array_merge($lsearch_array, search($lwhere, substr($l_remain, 1), $lexact_phrase, $lcase_sensitivity, $lsearch_array) ) ) ) );
+		}
+		return array_values( array_intersect($lid_array, $lsearch_array) );
+    }
+	
+	$current_user = new User($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
+    $user_perms = new User_Perms($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
+    $current_user_permission = new UserPermission($_SESSION['uid'], $GLOBALS['connection'], $GLOBALS['database']);
+    $s_getFTime = getmicrotime();
+    if($_REQUEST['where'] == 'author_locked_files')
+    {	$view_able_files_id = $current_user->getExpiredFileIds();}
+    else
+    {	$view_able_files_id = $current_user_permission->getViewableFileIds();}
+    $e_getFTime = getmicrotime();
+    $id_array_len = sizeof($view_able_files_id);
+    $query_array = array();
+    $search_result = search(@$_GET['where'], @$_GET['keyword'], @$_GET['exact_phrase'], @$_GET['case_sensitivity'], $view_able_files_id);
+    //echo 'khoa' . sizeof($search_result);
+    $page_url = $_SERVER['PHP_SELF'].'?keyword='.$_GET['keyword'].'&where='.$_GET['where'].'&submit='.$_GET['submit'];
+//    $sorted_obj_array = $current_user_permission->convertToFileDataOBJ($search_result);
+    //$sorted_obj_array = obj_array_sort_interface($search_result, $_GET['sort_order'], $_GET['sort_by']);
+	$sorted_result = my_sort($search_result, $_GET['sort_order'], $_GET['sort_by']);
+    list_files($sorted_result,  $current_user_permission, $page_url,  $GLOBALS['CONFIG']['dataDir'], $_GET['sort_order'], $_GET['sort_by'], $_GET['starting_index'], $_GET['stoping_index']);
+    echo '<BR>';
+    list_nav_generator(sizeof($search_result), $GLOBALS['CONFIG']['page_limit'], $GLOBALS['CONFIG']['num_page_limit'], $page_url,$_GET['page'], $_GET['sort_by'], $_GET['sort_order'] );
+    draw_footer();
+    echo '<br> <b> Load Page Time: ' . (getmicrotime() - $start_time) . ' </b>';
+    echo '<br> <b> Load Permission Time: ' . ($e_getFTime - $s_getFTime) . ' </b>';
 }
 ?>
