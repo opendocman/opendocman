dce3bb06f2bd222f8f1989c8d66c6c2a2faa3a3f
khoang82 <khoang82@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-06-19 22:51:20 +0000

    secureurl setup
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@283 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/config.php
+++ trunk/opendocman/config.php
@@ -7,36 +7,29 @@ if( !defined('config') )
 // config.php - useful variables/functions
 
 // database parameters
-$khoa = time();
-require_once 'functions.php';
-require_once 'ldap.inc';
-require_once 'classHeaders.php';
-require_once 'mimetypes.php';
+include 'functions.php';
+include 'classHeaders.php';
+include 'mimetypes.php';
 require_once('crumb.php');
 
 // Database Settings - Change these to match your database
-$database = 'opendocman_khoa'; // Enter the name of the database here
-$user = 'vault1'; // Enter the username for the database
-$pass = 'vault1'; // Enter the password for the username
-$hostname = 'musa'; // Enter the hostname that is serving the database
+$database = 'opendocman'; // Enter the name of the database here
+$user = 'opendocman'; // Enter the username for the database
+$pass = 'opendocman'; // Enter the password for the username
+$hostname = 'localhost'; // Enter the hostname that is serving the database
 
 
 global $CONFIG;      $CONFIG = array(
 'debug' => '0',
 
+// This setting is for a demo installation, where random people will be
+// all loggging in as the same username/password like 'demo/demo'.
+'demo' => 'false', 
+
 // This is useful if you have a web-based kerberos authenticatio site
 // Set to either kerbauth or mysql
 //'authen' => 'kerbauth',
 'authen' => 'mysql',
-
-// Not Working
-//Should we use ldap for user info lookup?
-// 'ldaplookup' => 'false',
-
-// Not Working
-//LDAP server (only needed if using ldaplookup
-//'ldap_server' => 'ldap.ucdavis.edu',
-//'ldap_basedn' => 'ou=University of California Davis,o=University of California,c=US',
 
 // Set the number of files that show up on each page
 'page_limit' => '15',
@@ -48,43 +41,51 @@ global $CONFIG;      $CONFIG = array(
 'displayable_len' => '15',
 
 // Set this to the url of the site
-'base_url' => 'http://cahfs.ucdavis.edu/~knguyen/cvs/opendocman1.2',
+'base_url' => 'http://mydomain/opendocman',
 
 // This is the browser window title
 'title' => 'Document Repository',
 
-// This is the program version for window title
-'current_version' => ' OpenDocMan v1.2rc1  ',
+// This is the program version for window title (This should be set to the current version of the program)
+'current_version' => ' OpenDocMan v1.2rc1',
 
 // The email address of the administrator of this site
-'site_mail' => 'admin@yourdomainaa.com',
+'site_mail' => 'admin@mydomain',
 
 //This variable sets the root username.  The root user will be able to access
 //all files and have authority for everything.
-'root_username'  => 'kdng',
+'root_username'  => 'admin',
 
 // location of file repository
 // this should ideally be outside the Web server root
 // make sure the server has permissions to read/write files!
-'dataDir' => '/usr/home/httpd/document_repository/', 
+'dataDir' => '/var/www/document_repository/', 
 
-//This var sets the amount of days until each file need to be revise, 
-//assuming that there are 30 days a month for all months.
+//This var sets the amount of days until each file needs to be revised, 
+//assuming that there are 30 days in a month for all months.
 'revision_expiration' => '90',
 
-/* Choose an action option when a file is found expired
+/* Choose an action option when a file is found to be expired
 The first two options also result in sending email to reviewer
  	(1) Remove from file list until renewed
 	(2) Show in file list but non-checkoutable
 	(3) Send email to reviewer only
 	(4) Do Nothing
 */
-'file_expired_action' => '1',
+'file_expired_action' => '1', 
 
 //Authorization control: On or Off (case sensitive)
-'authorization' => 'Off'
+'authorization' => 'On'
 );
 
+// List of allowed file types
+// Pay attention to the "Last Message:" in the status bar if your file is being rejected
+// because of its file type. It should display the proper MIME type there, and you can 
+// then add that string to this list to allow it
+$allowedFileTypes = array('image/gif', 'text/html', 'text/plain', 'application/pdf', 'application/x-pdf', 'application/x-lyx', 'application/msword', 'image/jpeg', 'image/pjpeg', 'image/png', 'application/msexcel', 'application/msaccess', 'text/richtxt', 'application/mspowerpoint', 'application/octet-stream', 'application/x-zip-compressed');
+
+// <----- No need to edit below here ---->
+//
 // Set the revision directory. (relative to $dataDir)
 $CONFIG['revisionDir'] = $GLOBALS['CONFIG']['dataDir'] . 'revisionDir/';
 
@@ -101,9 +102,8 @@ global $allowedFileTypes;
 
 $connection = mysql_connect($GLOBALS['hostname'], $GLOBALS['user'], $GLOBALS['pass']) or die ("Unable to connect!");
 $db = mysql_select_db($GLOBALS['database'], $GLOBALS['connection']);
-
-// list of allowed file types
-$allowedFileTypes = array('image/gif', 'text/html', 'text/plain', 'application/pdf', 'application/x-pdf', 'application/x-lyx', 'application/msword', 'image/jpeg', 'image/pjpeg', 'image/png', 'application/msexcel', 'application/msaccess', 'text/richtxt', 'application/mspowerpoint', 'application/octet-stream', 'application/x-zip-compressed');
 // All functions are in functions.php
+require_once 'secureurl.class.php';
+include 'secureurl.php';
 }
 ?>
