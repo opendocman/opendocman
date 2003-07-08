02017bda54aae8edc4c3d1b474d1397f5e7606eb
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-07-08 18:18:02 +0000

    seperated sql into seperate statements
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@300 769e0422-6c0b-0410-966c-d94082ee0ac6

--- trunk/opendocman/install/upgrade_11.php
+++ trunk/opendocman/install/upgrade_11.php
@@ -1,14 +1,13 @@
 <?php
 $result = mysql_query("
-#
-ALTER TABLE data
+ALTER TABLE `data`
     ADD filesize bigint(20) NULL DEFAULT NULL AFTER reviewer_comments,
     MODIFY category tinyint(4) unsigned NOT NULL DEFAULT '0',
     MODIFY status smallint(6) NULL DEFAULT NULL,
     ADD INDEX id (id),
     ADD INDEX `id_2` (id),
     ADD INDEX publishable (publishable),
-    ADD INDEX description (description);
+    ADD INDEX description (description)
 #
 #  Fieldformats of
 #    data.category changed from smallint(5) unsigned NOT NULL DEFAULT '0' to tinyint(4) unsigned NOT NULL DEFAULT '0'.
@@ -21,16 +20,20 @@ $result = mysql_query("
 ALTER TABLE `dept_perms`
     ADD INDEX rights (rights),
     ADD INDEX `dept_id` (`dept_id`),
-    ADD INDEX fid (fid);
+    ADD INDEX fid (fid)
+") or die("<br>Could not update dept_perms" . mysql_error());
 
+$result = mysql_query("
 ALTER TABLE log
     ADD revision varchar(255) NULL DEFAULT NULL AFTER note,
     ADD INDEX id (id),
-    ADD INDEX `modified_on` (`modified_on`);
+    ADD INDEX `modified_on` (`modified_on`)
+") or die("<br>Could not update log" . mysql_error());
 
+$result = mysql_query("
 ALTER TABLE `user_perms`
     ADD INDEX fid (fid),
     ADD INDEX uid (uid),
     ADD INDEX rights (rights);
-") or die("<br>Could not update" . mysql_error());
+") or die("<br>Could not update user_perms" . mysql_error());
 ?>
