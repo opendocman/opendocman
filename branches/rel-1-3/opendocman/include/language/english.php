f1ada4375adb680347b60e8f2f5508bb5095f827
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-07-23 18:56:02 +0000

    added admin and category words
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@334 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-3/opendocman/include/language/english.php
+++ branches/rel-1-3/opendocman/include/language/english.php
@@ -19,31 +19,79 @@ $lang['preferences'] = "Preferences";
 $lang['username'] = "Username";
 
 //Areas
+$lang['area_add_new_file'] = "Add New File";
+$lang['area_add_new_category'] = "Add New Category";
+$lang['area_admin'] = "Administration";
+$lang['area_delete_category'] = "Delete Category";
+$lang['area_display_category'] = "Display All Files In Category";
 $lang['area_document_listing'] = "Document Listing";
 $lang['area_file_delete'] = "Delete File";
+$lang['area_update_category'] = "Update Category";
 
 // Messages
+$lang['message_are_you_sure_remove'] = "Are you sure you want to remove this?";
+$lang['message_current'] = "Current";
+$lang['message_document_added'] = "Document successfully added";
 $lang['message_last_message'] = "Last Message:";
+$lang['message_folder_perms_error'] = "Folder Permissions Error:";
 $lang['message_for_further_assistance'] = "for further assistance";
+$lang['message_initial_import'] = "Initial Import";
 $lang['message_no_files_found'] = "No Files Found";
 $lang['message_no_description_available'] = "No description available";
+$lang['message_not_writeable'] = "Not Writeable!";
 $lang['message_please_email'] = "Please email";
+$lang['message_sorry_demo_mode'] = "Sorry, demo mode only, you can not do that!";
 $lang['message_unable_to_find_file'] = "Unable to find the requested file";
  
 // Labels
+$lang['label_add'] = "Add";
+$lang['label_admin'] = "Admin";
+$lang['label_all_departments'] = "All Departments";
 $lang['label_author'] = "Author";
 $lang['label_browse_by'] = "Browse by:";
+$lang['label_category'] = "Category";
+$lang['label_checked_out_files'] = "Checked-Out Files";
+$lang['label_comment'] = "Comment";
 $lang['label_created_date'] = "Created Date";
+$lang['label_delete'] = "Delete";
+$lang['label_default_for_unset'] = "Default Setting for Unset Department";
 $lang['label_department'] = "Department";
+$lang['label_department_authority'] = "Dept. Authority";
 $lang['label_description'] = "Description";
+$lang['label_display'] = "Display";
 $lang['label_empty'] = "Empty";
-$lang['label_file_category'] = "File Category";
+$lang['label_file_archive'] = "File Archive";
+$lang['label_file_location'] = "File Location";
+$lang['label_file_maintenance'] = "File Maintenance";
 $lang['label_file_name'] = "File Name";
 $lang['label_found_documents'] = "found document(s)";
+$lang['label_forbidden'] = "Forbidden";
+$lang['label_moderation'] = "Moderation";
+$lang['label_modify'] = "Modify";
 $lang['label_modified_date'] = "Modified Date";
+$lang['label_name'] = "Name";
+$lang['label_read'] = "Read";
+$lang['label_rejected_files'] = "Rejected Files";
 $lang['label_rights'] = "Rights";
+$lang['label_run_expiration'] = "Run Expiration Utility";
 $lang['label_select_one'] = "Select one";
+$lang['label_select_a_department'] = "Select a department";
 $lang['label_size'] = "Size";
+$lang['label_specific_permissions'] = "Specific User Permissions";
 $lang['label_status'] = "Status";
+$lang['label_update'] = "Update";
+$lang['label_user'] = "User";
+$lang['label_view'] = "View";
+
+// Buttons
+$lang['button_add_document'] = "Add Document";
+$lang['button_add_category'] = "Add Category";
+$lang['button_back'] = "Back";
+$lang['button_cancel'] = "Cancel";
+$lang['button_delete'] = "Delete";
+$lang['button_display_category'] = "Display All Files In This Category";
+$lang['button_modify_category'] = "Modify Category";
+$lang['button_update'] = "Update";
+$lang['button_yes'] = "Yes";
 
 ?>
