6407260be1aeea456f2de8e92610c9e90aed6c29
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-07-23 16:15:24 +0000

    added stuff for functions.php
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@329 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-3/opendocman/include/language/english.php
+++ branches/rel-1-3/opendocman/include/language/english.php
@@ -5,7 +5,45 @@
 $lang = array();
 global $lang;
 
+//Login
+$lang['welcome'] = "Welcome to OpenDocMan";
+$lang['welcome2'] = "Log in to begin using The system's powerful storage, publishing and revision control features.";
+
+
 //GLOBAL TERMS
-$lang['username'] = "Username";
+$lang['enter'] = "Enter";
+$lang['help'] = "Help";
+$lang['home'] = "Home";
 $lang['password'] = "Password";
+$lang['preferences'] = "Preferences";
+$lang['username'] = "Username";
+
+//Areas
+$lang['area_document_listing'] = "Document Listing";
+$lang['area_file_delete'] = "Delete File";
+
+// Messages
+$lang['message_last_message'] = "Last Message:";
+$lang['message_for_further_assistance'] = "for further assistance";
+$lang['message_no_files_found'] = "No Files Found";
+$lang['message_no_description_available'] = "No description available";
+$lang['message_please_email'] = "Please email";
+$lang['message_unable_to_find_file'] = "Unable to find the requested file";
+ 
+// Labels
+$lang['label_author'] = "Author";
+$lang['label_browse_by'] = "Browse by:";
+$lang['label_created_date'] = "Created Date";
+$lang['label_department'] = "Department";
+$lang['label_description'] = "Description";
+$lang['label_empty'] = "Empty";
+$lang['label_file_category'] = "File Category";
+$lang['label_file_name'] = "File Name";
+$lang['label_found_documents'] = "found document(s)";
+$lang['label_modified_date'] = "Modified Date";
+$lang['label_rights'] = "Rights";
+$lang['label_select_one'] = "Select one";
+$lang['label_size'] = "Size";
+$lang['label_status'] = "Status";
+
 ?>
