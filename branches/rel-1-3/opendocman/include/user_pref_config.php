59e862b878d4fd5cdc14cb1cfd127e64ac0f5198
khoang82 <khoang82@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-09-16 18:16:11 +0000

    setting for personal preference for ODM
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@349 769e0422-6c0b-0410-966c-d94082ee0ac6

---
+++ branches/rel-1-3/opendocman/include/user_pref_config.php
@@ -0,0 +1,12 @@
+<?php
+global $USER_PREF;
+$USER_PREF = array();
+$USER_PREF['language'] = array("English", "Spanish");
+$USER_PREF['color']['default'] = '#0000A0';
+$USER_PREF['color']['red'] = '#ff0000';
+$USER_PREF['color']['yellow'] = '#fcfc07';
+$USER_PREF['color']['blue'] = '#0000ff';
+$USER_PREF['color']['green'] = '#00ff00';
+$USER_PREF['color']['orange'] = '#fcfc07';
+$USER_PREF['color']['burgendy'] = '#990000';
+?>
