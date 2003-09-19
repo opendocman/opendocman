31b211baaf3ee5981a0b7c1793d2a8c14d5de023
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2003-09-19 21:39:49 +0000

    added sync.sh to help with keeping language files synced up Added README Added Spanish translation (Author:  Pablo Luis Zorzoli <pablozorzoli@yahoo.com.ar>)
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@382 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-3/opendocman/include/language/spanish.php
+++ branches/rel-1-3/opendocman/include/language/spanish.php
@@ -1,11 +1,96 @@
 <?php
 
 //SPANISH LANGUAGE FILE
-// 
+// Author:  Pablo Luis Zorzoli <pablozorzoli@yahoo.com.ar>
 $lang = array();
 global $lang;
 
+//Login
+$lang['welcome'] = "Bienvenido a OpenDocMan";
+$lang['welcome2'] = "Debe loguearse para comenzar a utilizar las esta poderosa herramienta de almacenamiento, publicaci&oacute;n y control de versiones";
+
 //GLOBAL TERMS
-$lang['username'] = "nombre de usuario";
-$lang['password'] = "contraseña";
+$lang['enter'] = "Ingresar";
+$lang['help'] = "Ayuda";
+$lang['home'] = "Principal";
+$lang['password'] = "Contrase&ntilde;a";
+$lang['profile'] = "Perfil";
+$lang['username'] = "Nombre de Usuario";
+
+//Areas
+$lang['area_add_new_file'] = "Agregar Archivo Nuevo";
+$lang['area_add_new_category'] = "Agregar Categor&iacute;a Nueva";
+$lang['area_admin'] = "Administraci&oacute;n";
+$lang['area_delete_category'] = "Eliminar Categor&iacute;a";
+$lang['area_display_category'] = "Mostrar Todos los Archivos en la Categor&iacute;a";
+$lang['area_document_listing'] = "Listado de Documentos";
+$lang['area_file_delete'] = "Eliminar Archivo";
+$lang['area_update_category'] = "Actualizar Categor&iacute;a";
+
+// Mensajes
+$lang['message_are_you_sure_remove'] = "Est&aacute; seguro que desea eliminarlo?";
+$lang['message_current'] = "Actual";
+$lang['message_document_added'] = "El Documento se agreg&oacute; con &eacute;xito";
+$lang['message_last_message'] = "&Uacute;ltimo Mensaje:";
+$lang['message_folder_perms_error'] = "Error de Permisos en la Carpeta:";
+$lang['message_for_further_assistance'] = "para m&aacute;s ayuda";
+$lang['message_initial_import'] = "Importaci&oacute;n Inicial";
+$lang['message_no_files_found'] = "No Se Encontraron Archivos";
+$lang['message_no_description_available'] = "Sin descripci&oacute;n disponible";
+$lang['message_not_writeable'] = "No se puede Escribir!";
+$lang['message_please_email'] = "Por favor su correo electr&oacute;nico";
+$lang['message_sorry_demo_mode'] = "Disculpe, este es el modo demostraci&oacute;n, esta operaci&oacute;n no est&aacute; permitida!";
+$lang['message_unable_to_find_file'] = "No se puede encontrar el archivo requerido";
+ 
+// Etiquetas
+$lang['label_add'] = "Agregar";
+$lang['label_admin'] = "Administrador";
+$lang['label_all_departments'] = "Todos los Sectores";
+$lang['label_author'] = "Autor";
+$lang['label_browse_by'] = "Examinar por:";
+$lang['label_category'] = "Categor&iacute;a";
+$lang['label_checked_out_files'] = "Archivos Retirados";
+$lang['label_comment'] = "Comentario";
+$lang['label_created_date'] = "Fecha de Creaci&oacute;n";
+$lang['label_delete'] = "Eliminar";
+$lang['label_default_for_unset'] = "Configuraci&iacute;n por defecto para Sector no definida";
+$lang['label_department'] = "Sector";
+$lang['label_department_authority'] = "Autoridad del Sector";
+$lang['label_description'] = "Descripci&oacute;n";
+$lang['label_display'] = "Mostrar";
+$lang['label_empty'] = "Vac&iacute;o";
+$lang['label_file_archive'] = "Registro de Archivos";
+$lang['label_file_location'] = "Ubicaci&oacute;n del Archivo";
+$lang['label_file_maintenance'] = "Mantenimieto de Archivos";
+$lang['label_file_name'] = "Nombre de Archivo";
+$lang['label_found_documents'] = "documento(s) encontrado(s)";
+$lang['label_forbidden'] = "Prohibido";
+$lang['label_moderation'] = "Moderaci&oacute;n";
+$lang['label_modify'] = "Modificar";
+$lang['label_modified_date'] = "Fecha de Modificaci&oacute;n";
+$lang['label_name'] = "Nombre";
+$lang['label_read'] = "Leer";
+$lang['label_rejected_files'] = "Archivos Rechazados";
+$lang['label_rights'] = "Permisos";
+$lang['label_run_expiration'] = "Ejecutar la Aplicaci&oacute;n de Control de Expiraci&oacute;n";
+$lang['label_select_one'] = "Seleccione uno";
+$lang['label_select_a_department'] = "Seleccione un sector";
+$lang['label_size'] = "Tama&ntilde;o";
+$lang['label_specific_permissions'] = "Permisos Espec&iacute;ficos del Usuario";
+$lang['label_status'] = "Estado";
+$lang['label_update'] = "Actualizar";
+$lang['label_user'] = "Usuario";
+$lang['label_view'] = "Ver";
+
+// Botones
+$lang['button_add_document'] = "Agregar Documento";
+$lang['button_add_category'] = "Agregar Categor&iacute;a";
+$lang['button_back'] = "Atr&aacute;s";
+$lang['button_cancel'] = "Cancelar";
+$lang['button_delete'] = "Eliminar";
+$lang['button_display_category'] = "Mostrar todos los archivos en esta categor&iacute;a";
+$lang['button_modify_category'] = "Modificar Categor&iacute;a";
+$lang['button_update'] = "Actualizar";
+$lang['button_yes'] = "Si";
+
 ?>
