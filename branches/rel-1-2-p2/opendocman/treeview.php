59b88289145e406e331ec8258427e64358f272d2
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2005-04-28 18:36:27 +0000

    fixed bug when no files listed
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@510 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-2-p2/opendocman/treeview.php
+++ branches/rel-1-2-p2/opendocman/treeview.php
@@ -11,31 +11,32 @@ function show_tree($fileid_array, $starting_index = 0, $stoping_index = 5) {
     if(!$tree){
         //echo "defining in browser\n";
         $tree = new Tree();
-        $root  = $tree->open_tree("Directory", "$PHP_SELF");
+        $root  = $tree->open_tree("Directory", $_SERVER['PHP_SELF']);
     }
 
     $TreeCategory = $_GET['TreeCategory'];
     $index = 0;
 
-    while($index<sizeof($fileid_array) and $index>=$starting_index and index<=$stoping_index)
+    if(isset($fileid_array['0']))
     {
-        $file_obj = new FileData($fileid_array[$index], $GLOBALS['connection'], $GLOBALS['database']);
-        $fileid = $file_obj->id;
-        $realname = $file_obj->getRealname();
-        $description = $file_obj->getDescription();
-        $modified_date = fix_date($file_obj->getModifiedDate());
-        $category = $file_obj->getCategoryName();
-        if(!$folders[$category]){
-            $folders[$category] = $tree->add_folder($root, "$category", 
-                    $PHP_SELF, "ftv2/ftv2folderclosed.gif", "ftv2/ftv2folderopen.gif");
+        while($index<sizeof($fileid_array) and $index>=$starting_index and $index<=$stoping_index)
+        {
+            $file_obj = new FileData($fileid_array[$index], $GLOBALS['connection'], $GLOBALS['database']);
+            $fileid = $file_obj->id;
+            $realname = $file_obj->getRealname();
+            $description = $file_obj->getDescription();
+            $modified_date = fix_date($file_obj->getModifiedDate());
+            $category = $file_obj->getCategoryName();
+            if(!$folders[$category])
+            {
+                $folders[$category] = $tree->add_folder($root, "$category",$_SERVER['PHP_SELF'], "ftv2/ftv2folderclosed.gif", "ftv2/ftv2folderopen.gif");
+            }
+
+            $tree->add_document($folders[$category], "$realname", "details.php?id=$fileid&state=2");
+            $index++;
         }
 
-
-        $tree->add_document($folders[$category], "$realname", "details.php?id=$fileid&state=2");
-        $index++;
+        $tree->close_tree ( );
     }
-
-    $tree->close_tree ( );
 }
-
 ?>
