4bb199a2d5a014b1c300901c1cc42268a0d8610a
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2005-04-28 03:34:40 +0000

    fixed missing brace
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@496 769e0422-6c0b-0410-966c-d94082ee0ac6

--- branches/rel-1-2-p2/opendocman/index.php
+++ branches/rel-1-2-p2/opendocman/index.php
@@ -50,9 +50,11 @@ if(isset($_POST['login']))
         // if MySQL login fails, check NIS/YP data
         if (mysql_num_rows($result) == 0)
         {
-            if (strcmp($cryptpw, $pwent[1]) == 0) {
-            $query = "SELECT id, username, password FROM user WHERE username = '$frmuser'";
-            $result = mysql_query("$query") or die ("Error in query: $query. " . mysql_error());
+            if (strcmp($cryptpw,$pwent[1]) == 0) 
+            {
+                $query = "SELECT id, username, password FROM user WHERE username = '$frmuser'";
+                $result = mysql_query("$query") or die ("Error in query: $query. " . mysql_error());
+            }
         }
         // if row exists - login/pass is correct
         if (mysql_num_rows($result) == 1)
@@ -177,10 +179,8 @@ elseif(!isset($_POST['login']) && $GLOBALS['CONFIG']['authen'] =='mysql')
 <?php
         draw_footer();
 }
-
 else
 {
         echo 'Check your config';
 }
-
 ?>
