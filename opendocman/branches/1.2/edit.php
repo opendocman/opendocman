491f835ff3a66b2457ea0d74c320f00f848017a7
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2006-11-17 07:39:51 +0000

    HTTP_VARS
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@540 769e0422-6c0b-0410-966c-d94082ee0ac6

--- opendocman/branches/1.2/edit.php
+++ opendocman/branches/1.2/edit.php
@@ -32,7 +32,7 @@ if(strchr($_REQUEST['id'], '_') )
 }
 if (!session_is_registered('uid'))
 {
-  header('Location:index.php?redirection=' . urlencode( $_SERVER['PHP_SELF'] . '?' . $HTTP_SERVER_VARS['QUERY_STRING'] ) );
+  header('Location:index.php?redirection=' . urlencode( $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'] ) );
   exit;
 }
 
