ac4a4f4dfa53e41509de1c60ba4e15c259b0f082
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2006-11-17 07:56:05 +0000

    version 1.2.4
    git-svn-id: http://opendocman.svn.sourceforge.net/svnroot/opendocman/opendocman/branches/1.2@542 769e0422-6c0b-0410-966c-d94082ee0ac6

--- config.php
+++ config.php
@@ -61,7 +61,7 @@ global $CONFIG;      $CONFIG = array(
 'title' => 'Document Repository',
 
 // This is the program version for window title (This should be set to the current version of the program)
-'current_version' => ' OpenDocMan v1.3',
+'current_version' => ' OpenDocMan v1.2.4',
 
 // The email address of the administrator of this site
 'site_mail' => 'root@localhost',
