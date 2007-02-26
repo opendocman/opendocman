73095ab7c59a54448298b76cb5f94c8cb21a429e
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2007-02-26 17:43:09 +0000

    added patch #[ 1638858 ] Configuration Option Handling change
    git-svn-id: http://opendocman.svn.sourceforge.net/svnroot/opendocman/opendocman/branches/1.2@577 769e0422-6c0b-0410-966c-d94082ee0ac6

--- functions.php
+++ functions.php
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 //require_once ('config.php');
+
+include_once 'version.php';
 include_once'classHeaders.php';
 include_once'mimetypes.php';
 require_once('crumb.php');
