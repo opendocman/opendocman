3a1a88c88ed0d14dab822064a3760b8dd76d2548
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2010-02-17 22:39:45 +0000

    1.2.5.6 tag creation
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@674 769e0422-6c0b-0410-966c-d94082ee0ac6

---
+++ opendocman/tags/rc/1.2.5.6/database.sql
@@ -0,0 +1,219 @@
+# MySQL dump 8.16
+#
+#--------------------------------------------------------
+# Server version	4.0.12-max-log
+
+#
+# Table structure for table 'odm_admin'
+#
+
+CREATE TABLE odm_admin (
+  id int(11) unsigned default NULL,
+  admin tinyint(4) default NULL
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_admin'
+#
+
+INSERT INTO odm_admin VALUES (1,1);
+
+#
+# Table structure for table 'odm_category'
+#
+
+CREATE TABLE odm_category (
+  id int(11) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_category'
+#
+
+INSERT INTO odm_category VALUES (1,'SOP');
+INSERT INTO odm_category VALUES (2,'Training Manual');
+INSERT INTO odm_category VALUES (3,'Letter');
+INSERT INTO odm_category VALUES (4,'Presentation');
+
+#
+# Table structure for table 'odm_data'
+#
+
+CREATE TABLE odm_data (
+  id int(11) unsigned NOT NULL auto_increment,
+  category tinyint(4) unsigned NOT NULL default '0',
+  owner int(11) unsigned default NULL,
+  realname varchar(255) NOT NULL default '',
+  created datetime NOT NULL default '0000-00-00 00:00:00',
+  description varchar(255) default NULL,
+  comment varchar(255) NOT NULL default '',
+  status smallint(6) default NULL,
+  department int(11) unsigned default NULL,
+  default_rights tinyint(4) default NULL,
+  publishable tinyint(4) default NULL,
+  reviewer int(11) unsigned default NULL,
+  reviewer_comments varchar(255) default NULL,
+  PRIMARY KEY  (id),
+  KEY data_idx (id,owner),
+  KEY id (id),
+  KEY id_2 (id),
+  KEY publishable (publishable),
+  KEY description (description)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_data'
+#
+
+#
+# Table structure for table 'odm_department'
+#
+
+CREATE TABLE odm_department (
+  id int(11) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_department'
+#
+
+INSERT INTO odm_department VALUES (1,'Information Systems');
+
+#
+# Table structure for table 'odm_dept_perms'
+#
+
+CREATE TABLE odm_dept_perms (
+  fid int(11) unsigned default NULL,
+  dept_id int(11) unsigned default NULL,
+  rights tinyint(4) NOT NULL default '0',
+  KEY rights (rights),
+  KEY dept_id (dept_id),
+  KEY fid (fid)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_dept_perms'
+#
+
+
+#
+# Table structure for table 'odm_dept_reviewer'
+#
+
+CREATE TABLE odm_dept_reviewer (
+  dept_id int(11) unsigned default NULL,
+  user_id int(11) unsigned default NULL
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_dept_reviewer'
+#
+
+INSERT INTO odm_dept_reviewer VALUES (1,1);
+
+#
+# Table structure for table 'odm_log'
+#
+
+CREATE TABLE odm_log (
+  id int(11) unsigned NOT NULL default '0',
+  modified_on datetime NOT NULL default '0000-00-00 00:00:00',
+  modified_by varchar(25) default NULL,
+  note text,
+  revision varchar(255) default NULL,
+  KEY id (id),
+  KEY modified_on (modified_on)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_log'
+#
+
+
+#
+# Table structure for table 'odm_rights'
+#
+
+CREATE TABLE odm_rights (
+  RightId tinyint(4) default NULL,
+  Description varchar(255) default NULL
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_rights'
+#
+
+INSERT INTO odm_rights VALUES (0,'none');
+INSERT INTO odm_rights VALUES (1,'view');
+INSERT INTO odm_rights VALUES (-1,'forbidden');
+INSERT INTO odm_rights VALUES (2,'read');
+INSERT INTO odm_rights VALUES (3,'write');
+INSERT INTO odm_rights VALUES (4,'admin');
+
+#
+# Table structure for table 'odm_user'
+#
+
+CREATE TABLE odm_user (
+  id int(11) unsigned NOT NULL auto_increment,
+  username varchar(25) NOT NULL default '',
+  password varchar(50) NOT NULL default '',
+  department int(11) unsigned default NULL,
+  phone varchar(20) default NULL,
+  Email varchar(50) default NULL,
+  last_name varchar(255) default NULL,
+  first_name varchar(255) default NULL,
+  pw_reset_code CHAR(32) default NULL,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_user'
+#
+
+INSERT INTO odm_user VALUES (1,'admin','',1,'5555551212','admin@example.com','User','Admin','');
+
+#
+# Table structure for table 'odm_user_perms'
+#
+
+CREATE TABLE odm_user_perms (
+  fid int(11) unsigned default NULL,
+  uid int(11) unsigned NOT NULL default '0',
+  rights tinyint(4) NOT NULL default '0',
+  KEY user_perms_idx (fid,uid,rights),
+  KEY fid (fid),
+  KEY uid (uid),
+  KEY rights (rights)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'odm_user_perms'
+#
+
+
+# New User Defined Fields Table
+#
+# field_type describes what type of UDF this is. At the momment
+# the valid values are:
+#
+#   1 = Drop down style list
+#   2 = Radio Buttons
+#
+# table_name names the database table where the allow values are listed
+#
+# display_name is the label shown to the user
+
+CREATE TABLE odm_udf
+(
+    id  int(11) auto_increment unique,
+    table_name  varchar(16),
+    display_name    varchar(16),
+    field_type  int
+) TYPE=MyISAM;
+
---
+++ opendocman/tags/rc/1.2.5.6/signup.php
@@ -0,0 +1,217 @@
+<?php
+/*
+add.php - adds files to the repository
+Copyright (C) 2002-2007  Stephen Lawrence, Jon Miner
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+
+// You can add signup_header.html and signup_footer.html files to display on this page automatically
+
+include('config.php');
+if($GLOBALS['CONFIG']['allow_signup'] == 'On')
+{
+
+    // Submitted so insert data now
+    if(isset($_REQUEST['adduser']))
+    {
+        // Check to make sure user does not already exist
+        $query = "SELECT username FROM {$GLOBALS['CONFIG']['db_prefix']}user WHERE username = '" . addslashes($_POST['username']) . '\'';
+        $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+
+        // If the above statement returns more than 0 rows, the user exists, so display error
+        if(mysql_num_rows($result) > 0)
+        {
+            echo 'That user already exists. Please <a href="signup.php">try again</a>';
+            exit;
+        }
+        else
+        {     
+            $phonenumber = @$_REQUEST['phonenumber'];
+            // INSERT into user
+            $query = "INSERT INTO {$GLOBALS['CONFIG']['db_prefix']}user (id, username, password, department, phone, Email,last_name, first_name) VALUES('', '". addslashes($_POST['username'])."', password('". addslashes(@$_REQUEST['password']) ."'), '" . addslashes($_REQUEST['department'])."' ,'" . addslashes($phonenumber) . "','". addslashes($_REQUEST['Email'])."', '" . addslashes($_REQUEST['last_name']) . "', '" . addslashes($_REQUEST['first_name']) . '\' )';
+            $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+            // INSERT into admin
+            $userid = mysql_insert_id($GLOBALS['connection']);
+            if (!isset($_REQUEST['admin']))
+            {
+                $_REQUEST['admin']='';
+            }
+            $query = "INSERT INTO {$GLOBALS['CONFIG']['db_prefix']}admin (id, admin) VALUES('$userid', '$_REQUEST[admin]')";
+            $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+            if(isset($_REQUEST['reviewer']))
+            {
+                for($i = 0; $i<sizeof($_REQUEST['department_review']); $i++)
+                {
+                    $dept_rev=$_REQUEST['department_review'][$i];
+                    $query = "INSERT INTO {$GLOBALS['CONFIG']['db_prefix']}dept_reviewer (dept_id, user_id) values('$dept_rev', $userid)";
+                    $result = mysql_query($query, $GLOBALS['connection']) or die("Error in query: $query". mysql_error());
+                }
+            }
+
+            // mail user telling him/her that his/her account has been created.
+            echo 'Your account has been created<br>';
+            if($GLOBALS['CONFIG']['authen'] == 'mysql')
+            {
+                echo 'Your randomly generated passowrd is: '.$_REQUEST['password']."\n\n";
+                echo '<br>If you would like to change this to something else once you log in, ';
+                echo 'you can do so by clicking on "Preferences" in the status bar.'."\n";
+                echo '<br><a href="' . $GLOBALS['CONFIG']['base_url'] . '">Login</a>';
+                exit;
+            }
+            else 
+                $mail_body.='Your password is your UC Davis campus kerberos password.';
+            $mail_salute="\n\rSincerely,\n\r$full_name";
+            mail($mail_to, $mail_subject, ($mail_greeting.' '.$mail_body.$mail_salute), $mail_headers);
+        }
+    }
+    elseif(isset($_REQUEST['updateuser']))
+    {
+        if(!isset($_REQUEST['admin']) || $_REQUEST['admin'] == '')
+        {
+            $_REQUEST['admin'] = 'no';
+        }
+
+        if(!isset($_REQUEST['caller']) || $_REQUEST['caller'] == '')
+        {
+            $_REQUEST['caller'] = 'admin.php';
+        }
+
+        $user_obj = new User($_REQUEST['id'], $GLOBALS['connection'], $GLOBALS['database']);
+
+        // UPDATE admin info
+        $query = "UPDATE {$GLOBALS['CONFIG']['db_prefix']}admin set admin='". $_REQUEST['admin'] . "' where id = '".$_REQUEST['id']."'";
+        $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+        // UPDATE into user
+        $query = "UPDATE {$GLOBALS['CONFIG']['db_prefix']}user SET username='". addslashes($_POST['username']) ."',";
+        if (!empty($_REQUEST['password']))
+        {
+            $query .= "password = password('". addslashes($_REQUEST['password']) ."'), ";
+        }
+        if( isset( $_REQUEST['department'] ) )
+        {	$query.= 'department="' . addslashes($_REQUEST['department']) . '",';	}
+        if( isset( $_REQUEST['phonenumber'] ) )
+        {	$query.= 'phone="' . addslashes($_REQUEST['phonenumber']) . '",';	}
+        if( isset( $_REQUEST['Email'] ) )
+        {	$query.= 'Email="' . addslashes($_REQUEST['Email']) . '" ,';	}
+        if( isset( $_REQUEST['last_name'] ) )
+        {	$query.= 'last_name="' . addslashes($_REQUEST['last_name']) . '",';	}
+        if( isset( $_REQUEST['first_name'] ) )
+        {	$query.= 'first_name="' . addslashes($_REQUEST['first_name']) . '" ';	}
+        $query.= 'WHERE id="' . $_REQUEST['id'] . '"';
+        $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+
+        // UPDATE into dept_reviewer
+        $query = "DELETE FROM {$GLOBALS['CONFIG']['db_prefix']}dept_reviewer where user_id = '$_REQUEST[id]'";
+        $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());  
+        if(isset($_REQUEST['reviewer']))
+        {
+            //Remove all entry for $id
+            $query = "DELETE FROM {$GLOBALS['CONFIG']['db_prefix']}dept_reviewer where user_id = $_REQUEST[id]";
+            $result = mysql_query($query, $GLOBALS['connection']) or die("Error in query: $query". mysql_error());
+            $depts_rev = $_REQUEST['department_review'];
+            for($i = 0; $i<sizeof($_REQUEST['department_review']); $i++)
+            {
+                $dept_rev=$depts_rev[$i];
+                $query = "INSERT INTO {$GLOBALS['CONFIG']['db_prefix']}dept_reviewer (dept_id, user_id) values('$dept_rev', $_REQUEST[id])";
+                $result = mysql_query($query, $GLOBALS['connection']) or die("Error in query: $query". mysql_error());
+            }
+        }
+        // back to main page
+        if(!isset($_REQUEST['caller']))
+        {	$_REQUEST['caller'] = 'admin.php';	}
+        $_REQUEST['last_message'] = urlencode('User successfully updated');
+        header('Location: ' . $_REQUEST['caller'] . '?last_message=' . $_REQUEST['last_message']);
+    }
+    ?>
+        <html>
+        <head><title>Sign Up</title></head>
+        <body>
+<?php
+    if (is_readable("signup_header.html"))
+      include("signup_header.html");
+?>
+        <center><font size=6>Sign Up</font></center>
+        <br><SCRIPT LANGUAGE="JavaScript1.2" src="FormCheck.js"></script>			   
+
+        <center>
+        <table border="0" cellspacing="5" cellpadding="5">
+        <form name="add_user" action="signup.php" method="POST" enctype="multipart/form-data">
+        <tr><td><b>Last Name</b></td><td><input name="last_name" type="text"></td></tr>
+        <tr><td><b>First Name</b></td><td><input name="first_name" type="text"></td></tr>
+        <tr><td><b>Username</b></td><td><input name="username" type="text"></td></tr>
+        <tr>
+        <td><b>Phone Number</b></td>
+        <td>
+        <input name="phonenumber" type="text">
+        </td>
+        </tr>
+        <tr>
+        <td><b>Example</b></td>
+        <td><b>999 9999999</b></td>
+        </tr>
+        <tr>
+        <td><b>E-mail Address</b></td>
+        <td>
+        <input name="Email" type="text">
+        </td>
+        </tr>
+        <tr>
+        <?php
+        // If mysqlauthentication, then ask for password
+        if( $GLOBALS['CONFIG']['authen'] =='mysql')
+        {
+            $rand_password = makeRandomPassword(); 
+            echo '<INPUT type="hidden" name="password" value="' . $rand_password . '">';
+        }
+    ?>
+
+        <tr>
+        <td><b>Department</b></td>
+        <td>
+        <select name="department">
+        <?php			
+        // query to get a list of departments
+        $query = "SELECT id, name FROM {$GLOBALS['CONFIG']['db_prefix']}department ORDER BY name";
+    $result = mysql_query($query, $GLOBALS['connection']) or die ("Error in query: $query. " . mysql_error());
+
+    while(list($id, $name) = mysql_fetch_row($result))
+    {
+        echo '<option value=' . $id . '>' . $name . '</option>';
+    }
+
+    mysql_free_result ($result);
+    ?>
+        </select>
+        </td>
+        <tr>
+        <td></td>
+        <td columnspan=3 align="center"><input type="Submit" name="adduser" onClick="return validatemod(add_user);" value="Sign Up!">
+        </form>
+        </td>
+        </tr>
+        </table>
+        </center>
+<?php
+   if (is_readable("signup_footer.html"))
+    include("signup_footer.html");
+?>
+
+        </body>
+        </html>
+        <?php
+}
+?>
