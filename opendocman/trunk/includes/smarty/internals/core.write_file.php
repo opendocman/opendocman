b0e42e5dbb278141c7a677f89a0bda68b42a42b7
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2010-06-01 17:34:08 +0000

    2.6.26 Smarty 0000294: Update smarty
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@718 769e0422-6c0b-0410-966c-d94082ee0ac6

--- opendocman/trunk/includes/smarty/internals/core.write_file.php
+++ opendocman/trunk/includes/smarty/internals/core.write_file.php
@@ -37,7 +37,7 @@ function smarty_core_write_file($params, &$smarty)
     fwrite($fd, $params['contents']);
     fclose($fd);
 
-    if (PHP_OS == 'Windows' || !@rename($_tmp_file, $params['filename'])) {
+    if (DIRECTORY_SEPARATOR == '\\' || !@rename($_tmp_file, $params['filename'])) {
         // On platforms and filesystems that cannot overwrite with rename() 
         // delete the file before renaming it -- because windows always suffers
         // this, it is short-circuited to avoid the initial rename() attempt
