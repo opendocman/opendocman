66d3493e337f01ff7732dbf30f26053499d324a7
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2011-05-03 15:26:17 +0000

    0000386: Translate to Portuguese 0000385: Translate to Croatian
    git-svn-id: https://opendocman.svn.sourceforge.net/svnroot/opendocman@810 769e0422-6c0b-0410-966c-d94082ee0ac6

---
+++ opendocman/trunk/includes/language/croatian.php
@@ -0,0 +1,327 @@
+<?php
+//CROATIAN LANGUAGE FILE
+// (C) 20010-2011 Stephen Lawrence Jr.
+// Translated by: Mihovil Stanić
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+$lang = array();
+global $lang;
+
+//Login
+$lang['anonymous_link'] = 'Kliknite ovdje kako bi ste anonimno gledali javne datoteke';
+$lang['forgotpassword'] = 'Zaboravili ste lozinku?';
+$lang['install_folder']  = 'Sigurnosno upozorenje: Ako ste već instalirali/nadogradili program, trebali bi ste izbrisati \'install\' direktorij prije nego nastavite dalje';
+$lang['signup'] = 'Registrirajte račun';
+$lang['welcome'] = 'Dobro došli u OpenDocMan';
+$lang['welcome2'] = 'Prijavite se kako bi ste počeli koristiti moćnu sistemsku pohranu podataka, objavljivanje i kontrolu promjena nad datotekama.';
+$lang['welcome_anonymous_title'] = 'OpenDocMan Anonimna Stranica: Prikaži sve';
+$lang['welcome_anonymous_h1'] = 'OpenDocMan Anonimna Stranica';
+
+//GLOBAL TERMS
+$lang['action'] = 'Akcija';
+$lang['all'] = 'Sve';
+$lang['anonymous'] = 'Anonimno';
+$lang['author'] = 'Autor';
+$lang['category'] = 'Kategorija';
+$lang['choose'] = 'Izaberi';
+$lang['date'] = 'Datum';
+$lang['days'] = 'dana';
+$lang['department'] = 'Odjel';
+$lang['edit'] = 'Promjeni';
+$lang['enter'] = 'Uđi';
+$lang['error'] = 'Greška';
+$lang['file'] = 'Datoteka';
+$lang['help'] = 'Pomoć';
+$lang['home'] = 'Početna';
+$lang['installer'] = 'Instalacija';
+$lang['list'] = 'Lista';
+$lang['login'] = 'Prijava';
+$lang['logout'] = 'Odjava';
+$lang['new'] = 'Novi';
+$lang['owner'] = 'Vlasnik';
+$lang['preferences'] = 'Opcije';
+$lang['password'] = 'Lozinka';
+$lang['please'] = 'Molim';
+$lang['profile'] = 'Profil';
+$lang['public'] = 'Javno';
+$lang['revision'] = 'Revizija';
+$lang['search'] = 'Pretraži';
+$lang['select'] = 'Izaberi';
+$lang['sincerely'] = 'Iskreno';
+$lang['submit'] = 'Pošalji';
+$lang['time'] = 'Vrijeme';
+$lang['type'] = 'Tip';
+$lang['updated'] = 'Obnovljeno';
+$lang['username'] = 'Korisničko ime';
+$lang['users'] = 'Korisnici';
+$lang['value'] = 'Vrijednost';
+$lang['view'] = 'Pogledaj';
+
+// Areas
+$lang['area_add_new_category'] = 'Dodaj novu kategoriju';
+$lang['area_add_new_department'] = 'Dodaj novi odjel';
+$lang['area_add_new_file'] = 'Dodaj novu datoteku';
+$lang['area_add_new_udf'] = 'Dodaj novo korisnicko polje';
+$lang['area_add_new_user'] = 'Dodaj novog korisnika';
+$lang['area_admin'] = 'Administracija';
+$lang['area_check_in_file'] = 'Prijavi datoteku';
+$lang['area_check_out_file'] = 'Odjavi datoteku';
+$lang['area_choose_department'] = 'Izaberi odjel';
+$lang['area_delete_category'] = 'Izbriši kategoriju';
+$lang['area_deleted_files'] = 'Izbrisane datoteke';
+$lang['area_department_information'] = 'Informacije o odjelu';
+$lang['area_display_category'] = 'Prikaži sve datoteke u kategoriji';
+$lang['area_document_listing'] = 'Lista dokumenata';
+$lang['area_file_delete'] = 'Izbriši datoteku';
+$lang['area_file_details'] = 'Detalji o datoteci';
+$lang['area_file_expiration'] = 'Istek trajanja datoteke';
+$lang['area_personal_profile'] = 'Osobni profil';
+$lang['area_reset_password'] = 'Resetiraj lozinku';
+$lang['area_update_category'] = 'Promjeni kategoriju';
+$lang['area_update_department'] = 'Promjeni odjel';
+$lang['area_update_file'] = 'Promjeni datoteku';
+$lang['area_view_category'] = 'Pogledaj kategoriju';
+
+// Buttons
+$lang['button_add_category'] = 'Dodaj kategoriju';
+$lang['button_add_department'] = 'Dodaj odjel';
+$lang['button_add_document'] = 'Dodaj dokument';
+$lang['button_authorize'] = 'Autoriziraj';
+$lang['button_back'] = 'Nazad';
+$lang['button_cancel'] = 'Odustani';
+$lang['button_check_in'] = 'Prijavi';
+$lang['button_clear_status'] = 'Očisti status';
+$lang['button_click_here'] = 'Klikni ovdje';
+$lang['button_continue'] = 'Nastavi';
+$lang['button_delete'] = 'Obriši';
+$lang['button_delete_files'] = 'Obriši datoteke';
+$lang['button_display_category'] = 'Prikaži sve datoteke u ovoj kategoriji';
+$lang['button_modify_category'] = 'Promjeni kategoriju';
+$lang['button_modify_department'] = 'Promjeni odjel';
+$lang['button_reject'] = 'Odbaci';
+$lang['button_reset'] = 'Resetiraj';
+$lang['button_resubmit_for_review'] = 'Ponovo pošalji na pregled';
+$lang['button_save'] = 'Sačuvaj';
+$lang['button_undelete'] = 'Po-vrati datoteke';
+$lang['button_update'] = 'Obnovi';
+$lang['button_view_department'] = 'Pogledaj odjel';
+$lang['button_yes'] = 'Da';
+
+//Email
+$lang['email_added_to_repository'] = 'dodano u repozitorij';
+$lang['email_a_new_file_has_been_added'] = 'Nova datoteka je dodana';
+$lang['email_a_new_file_has_been_rejected'] = 'Nova datoteka je odbijena';
+$lang['email_automated_document_messenger'] = 'Automatizirani sustav poruka';
+$lang['email_because_you_did_not_revise'] = 'zato što ga niste izmjenili više od';
+$lang['email_comments_regarding_review'] = 'Komentari na vaš pregled dokumenta';
+$lang['email_email_all_users'] = 'Pošalji e-mail svim korisnicima';
+$lang['email_email_these_users'] = 'Pošalji e-mail ovim korisnicima';
+$lang['email_email_whole_department'] = 'Pošalji e-mail cijelom odjelu';
+$lang['email_file_expired'] = 'Datoteci je istekao rok';
+$lang['email_file_was_rejected_because'] = 'Vaša datoteka je odbijena zato što je niste izmjenili više od';
+$lang['email_file_was_rejected_expired'] = 'Vašoj datoteci je istekao rok. Molimo obnovite datoteku što je prije moguće. Vaša datoteka možda neće biti dostupna dok je ne obnovite.';
+$lang['email_for_the_following_reasons'] = 'iz slijedećih razloga';
+$lang['email_greeting'] = 'Dragi autore';
+$lang['email_i_would_like_to_inform'] = 'Želio bih vas obavjestiti da';
+$lang['email_note_to_authors'] = 'Poruka autoru(ima)';
+$lang['email_revision_days'] = 'Maksimalni broj dana prije isticanja roka:';
+$lang['email_salute'] = 'Srdačan';
+$lang['email_someone_has_requested_password'] = 'Netko je zatražio resetiranje lozinke.  Ako želite resetirati lozinku molimo pratite link ispod.  Ako ne želite resetirati svoju lozinku nemojte ništa raditi i zanemarite ovaj e-mail.';
+$lang['email_status_expired'] = 'Status: Istekao rok';
+$lang['email_subject_review_status'] = 'Status pregleda dokumenta ';
+$lang['email_subject'] = 'Naslov';
+$lang['email_thank_you'] = 'Hvala';
+$lang['email_to'] = 'Prima';
+$lang['email_was_declined_for_publishing_at'] = 'Odbijen je za objavljivanje u';
+$lang['email_was_rejected_from_repository'] = 'Odbijen je iz repozitorija';
+$lang['email_you_can_now_login'] = 'Možete se prijaviti u svoj račun na ovoj stranici';
+$lang['email_your_account_created'] = 'vaš račun za upravljanje dokumenti je napravljen u';
+$lang['email_your_file_has_been_authorized'] = 'Vaša datoteka je odobrena za objavljivanje';
+
+// Labels
+$lang['label_active'] = 'Aktivno';
+$lang['label_add'] = 'Dodaj';
+$lang['label_admin'] = 'Administrator';
+$lang['label_allowed'] = 'Dozvoljeno';
+$lang['label_all_departments'] = 'Svi odjeli';
+$lang['label_assign_to'] = 'Dodjeli korisniku';
+$lang['label_author'] = 'Autor';
+$lang['label_browse_by'] = 'Filtriraj po:';
+$lang['label_case_sensitive'] = 'Osjetljivo na velika/mala slova';
+$lang['label_checked_out_files'] = 'Datoteke koje su izdane';
+$lang['label_check_expiration'] = 'Provjeri istek roka trajanja';
+$lang['label_comment'] = 'Komentar';
+$lang['label_created_date'] = 'Datum kreiranja';
+$lang['label_default_for_unset'] = 'Standardne postavke kada odjel nije odabran';
+$lang['label_delete'] = 'Izbriši';
+$lang['label_delete_undelete'] = 'Izbriši/Vrati izbrisano';
+$lang['label_department_authority'] = 'Ovlasti odjela';
+$lang['label_department'] = 'Odjel';
+$lang['label_departments'] = 'Odjeli';
+$lang['label_department_to_modify'] = 'Izmjeniti odjel';
+$lang['label_description'] = 'Opis';
+$lang['label_display'] = 'Prikaži';
+$lang['label_email_address'] = 'E-Mail adresa';
+$lang['label_empty'] = 'Prazno';
+$lang['label_exact_phrase'] = 'Točni izraz';
+$lang['label_example'] = 'Primjer';
+$lang['label_file_archive'] = 'Arhiva datoteka';
+$lang['label_file_category'] = 'Kategorija datoteka';
+$lang['label_file_listing'] = 'Lista datoteka';
+$lang['label_file_location'] = 'Lokacija datoteke';
+$lang['label_file_maintenance'] = 'Održavanje datoteke';
+$lang['label_filename'] = 'Naziv datoteke';
+$lang['label_filetype'] = 'Tip datoteke';
+$lang['label_filetypes'] = 'Tipovi datoteka';
+$lang['label_file_name'] = 'Naziv datoteke';
+$lang['label_first_name'] = 'Ime';
+$lang['label_forbidden'] = 'Zabranjeno';
+$lang['label_found_documents'] = 'pronašao dokument(e)';
+$lang['label_id'] = 'BROJ';
+$lang['label_is_admin'] = 'Je administrator';
+$lang['label_is_reviewer'] = 'Je pregledavač';
+$lang['label_last_name'] = 'Prezime';
+$lang['label_moderation'] = 'Moderiranje';
+$lang['label_modified_date'] = 'Datum promjene';
+$lang['label_modify'] = 'Promjeni';
+$lang['label_name'] = 'Naziv';
+$lang['label_new_password'] = 'Nova lozinka';
+$lang['label_next'] = 'Slijedeći';
+$lang['label_note_for_revision_log'] = 'Poruka za revizijski dnevnik';
+$lang['label_page'] = 'Stranica';
+$lang['label_phone_number'] = 'Broj telefona';
+$lang['label_plugins'] = 'Dodaci';
+$lang['label_prev'] = 'Prošli';
+$lang['label_read'] = 'Čitaj';
+$lang['label_reassign_to'] = 'Ponovo dodjeli korisniku';
+$lang['label_rejected_files'] = 'Odbijene datoteke';
+$lang['label_rejections'] = 'Odbijenice';
+$lang['label_reviewer_for'] = 'Pregledavač za odjel';
+$lang['label_reviewer'] = 'Pregledavač';
+$lang['label_reviews'] = 'Pregledani';
+$lang['label_rights'] = 'Dozvole';
+$lang['label_run_expiration'] = 'Pokreni program za rokove datoteka';
+$lang['label_search_term'] = 'Traži pojam';
+$lang['label_select_a_department'] = 'Odaberi odjel';
+$lang['label_select_departments'] = 'Odaberi odjele';
+$lang['label_select_one'] = 'Izaberi jedan';
+$lang['label_settings'] = 'Postavke';
+$lang['label_size'] = 'Veličina';
+$lang['label_specific_permissions'] = 'Specifične korisničke dozvole (koristi ctrl-klik ili cmd-klik)';
+$lang['label_status'] = 'Status';
+$lang['label_update'] = 'Obnovi';
+$lang['label_user_defined_fields'] = 'Korisnički definirana polja';
+$lang['label_user_defined_field'] = 'Korisnički definirano polje';
+$lang['label_users_in_department'] = 'Korisnici u ovom odjelu';
+$lang['label_user'] = 'Korisnik';
+$lang['label_view'] = 'Pogledaj';
+
+// Messages
+$lang['message_account_created_add_user'] = 'Vaš račun je kreiran.';
+$lang['message_account_created_password'] = 'Vaša sustavom generirana lozinka je';
+$lang['message_account_created'] = 'Vaš račun je kreiran. Molimo provjerite svoj e-mail za informacije o prijavi u sustav.';
+$lang['message_action_cancelled'] = 'Radnja poništena';
+$lang['message_all_actions_successfull'] = 'Sve radnje su završene uspješno';
+$lang['message_an_email_has_been_sent'] = 'E-mail je poslan na vašu email adresu sa linkom koji morate posjetiti kako bi resetirali lozinku.';
+$lang['message_anonymous_view'] = 'Prebačeni ste u anonimni mod za gledanje';
+$lang['message_are_you_sure_remove'] = 'Da li ste sigurni da želite to ukloniti?';
+$lang['message_authorized'] = 'Odobreno';
+$lang['message_category_successfully_added'] = 'Kategorija uspješno dodana';
+$lang['message_category_successfully_deleted'] = 'Kategorija uspješno izbrisana';
+$lang['message_category_successfully_updated'] = 'Kategorija uspješno izmjenjena';
+$lang['message_click_to_checkout_document'] = 'Kliknite kako bi ste odjavili odabrani dokument i počeli ga skidati na vaš kompjuter';
+$lang['message_config_value_problem'] = 'Postoji problem sa jednom od konfiguriranih opcija. Molimo provjerite.';
+$lang['message_current'] = 'Trenutno';
+$lang['message_datadir_problem'] = 'Postoji problem sa vašim dataDir. Provjerite da li postoji i da se u njega može zapisivati';
+$lang['message_datadir_problem_exists'] = 'Postoji problem sa postavkama dataDir. Direktorij nije pronađen, provjerite da li postoji.';
+$lang['message_datadir_problem_writable'] = 'Postoji problem sa postavkama dataDir. U direktorij nije moguće vršiti zapisivanje.';
+$lang['message_department_successfully_added'] = 'Odjel uspješno dodan';
+$lang['message_department_successfully_updated'] = 'Odjel uspješno izmjenjen';
+$lang['message_directory_creation_failed'] = 'Kreiranje direktorija neuspješno';
+$lang['message_document_added'] = 'Dokument uspješno dodan';
+$lang['message_document_checked_in'] = 'Dokument uspješno prijavljen';
+$lang['message_document_checked_out_to_you'] = 'Lista trenutno odjavljenih dokumenata za vaš korisnički račun';
+$lang['message_document_has_been_archived'] = 'Dokument je arhiviran';
+$lang['message_documents_expired'] = 'Dokumenti sa isteklim rokom';
+$lang['message_documents_rejected'] = 'Odbijeni dokumenti';
+$lang['message_document_successfully_deleted'] = 'Dokument uspješno izbrisan';
+$lang['message_documents_waiting'] = 'Dokumenti na čekanju za odobrenje';
+$lang['message_error_performing_action'] = 'Pojavila se greška u izvođenju tražene radnje';
+$lang['message_file_authorized'] = 'Odobravanje datoteke uspješno izvršeno';
+$lang['message_file_does_not_exist'] = 'Datoteka ne postoji ili postoji neki problem sa datotekom.';
+$lang['message_file_expired'] = 'Istekao rok trajanja datoteke';
+$lang['message_file_rejected'] = 'Odbacivanje datoteke uspješno dovršeno';
+$lang['message_folder_error_check'] = 'Greška u direktoriju. Provjerite zadnju poruku u statusnoj traci za više detalja.';
+$lang['message_folder_perms_error'] = 'Greška u dozvolama direktorija:';
+$lang['message_for_further_assistance'] = 'za daljnu pomoć';
+$lang['message_found_documents'] = 'pronašao dokument(e)';
+$lang['message_if_you_are_unable_to_view'] = 'Ako iz nekog razloga niste u mogućnosti, kliknite <input type="submit" name="submit" value="Skini"> kako bi ste skinuli odabrani dokument i sačuvali ga na vaš kompjuter.';
+$lang['message_initial_import'] = 'Početni uvoz';
+$lang['message_last_message'] = 'Posljednja poruka';
+$lang['message_latest_version'] = 'Najnovija verzija';
+$lang['message_need_one_department'] = 'Potrebno je odabrati barem jedan odjel';
+$lang['message_no_author_comments_available'] = 'Autorov komentar nije dostupan';
+$lang['message_no_description_available'] = 'Opis nije dostupan';
+$lang['message_no_documents_checked_out'] = 'Trenutno niti jedan dokument nije vama izdan.';
+$lang['message_no_files_found'] = 'Datoteke nisu pronađene';
+$lang['message_no_information_available'] = 'Informacija nije dostupna';
+$lang['message_non_unique_account'] = 'Račun nije jedinstven';
+$lang['message_non_unique_key'] = 'Ključ u bazi nije jedinstven.';
+$lang['message_nothing_to_do'] = 'Ništa za raditi';
+$lang['message_not_writeable'] = 'Nije moguće zapisati!';
+$lang['message_once_the_document_has_completed'] = 'Kada se završi skidanje dokumenta, možete';
+$lang['message_original_version'] = 'Originalna verzija';
+$lang['message_please_email'] = 'Molimo pošaljite e-mail';
+$lang['message_please_upload_valid_doc'] = 'Molimo učitajte ispravan dokument';
+$lang['message_record_exists'] = 'Unos već postoji. Probajte ponovo sa drugom vrijednosti.';
+$lang['message_rejected'] = 'Odbijeno';
+$lang['message_rejecting_files'] = 'Odbacujem datoteke koje su zadnji puta promjenjene prije';
+$lang['message_reviewers_comments_re_rejection'] = 'Pregledavačev komentar glede odbacivanja';
+$lang['message_session_error'] = 'Greška sesije. Molimo prijavite se ponovo.';
+$lang['message_set_your_new_password'] = 'Postavite novu lozinku koristeći formular ispod.';
+$lang['message_sorry_demo_mode'] = 'Žao nam je, demo verzija samo, ne možete to uraditi!';
+$lang['message_sorry_not_allowed'] = 'Žao nam je, nije vam dozvoljeno to uraditi!';
+$lang['message_that_filetype_not_supported'] = 'Taj tip datoteke trenutno nije podržan.<p>Molimo učitajte dokument koji odgovara jednom od niže navedenih tipova dokumenata ili dodajte tip dokumenta ( MIMETYPE ) u ';
+$lang['message_the_code_you_are_using'] = 'Kod koji pokušavate iskoristiti za resetiranje vaše lozinke više nije vrijedeći.  Molimo koristite ovaj formular kako bi ste resetirali lozinku.';
+$lang['message_the_file_is_too_large'] = 'Datoteka je prevelika. Molimo provjerite postavke programa OpenDocMan. Maksimalna veličina je';
+$lang['message_the_file_is_too_large_php_ini'] = 'Datoteka je prevelika za vaš php server. Provjerite svoju php.ini konfiguraciju za maksimalnu veličinu. Trenutna maksimalna veličina je: ';
+$lang['message_there_was_an_error_loggin_you_in'] = 'Pojavila se greška u prijavi u sustav. Molimo pokušajte ponovo.';
+$lang['message_there_was_an_error_performing_the_action'] = 'Pojavila se greška u izvođenju tražene radnje.';
+$lang['message_the_username_you_entered'] = 'Korisničko ime koje ste unijeli nije pronađeno u našem sistemu. Kontaktirajte nas ako ste zaboravili svoje korisničko ime.';
+$lang['message_this_file_cannot_be_checked_in'] = 'Ova datoteka ne može biti prijavljena';
+$lang['message_this_file_cannot_be_uploaded'] = 'Ova datoteka ne može biti ispravno učitana';
+$lang['message_this_operation_cannot_be_done_file'] = 'Ova operacija se ne može izvrišiti na toj datoteci';
+$lang['message_this_operation_cannot_be_done_rev'] = 'Ova operacija se ne može koristiti na reviziji datoteke';
+$lang['message_this_page_requires_root'] = 'Ova stranica zahtjeva root razinu dozvola';
+$lang['message_this_site_has_high_security'] = 'Ova stranica koristi visoku razinu zaštite i ne možemo dohvatiti vašu lozinku za vas. Možete koristiti ovaj formular kako bi ste resetirali svoju lozinku.  Unesite svoje korisničko ime i poslat ćemo vam e-mail sa web adresom koju morate pratiti kako bi ste resetirali svoju lozinku.  Tek tada možete postaviti željenu lozinku sami.';
+$lang['message_to_view_your_file'] = 'Kako bi ste vidjeli svoju datoteku u novom prozoru';
+$lang['message_udf_cannot_be_blank'] = 'Ime korisnički definiranog polja ne može biti prazno';
+$lang['message_udf_successfully_added'] = 'Korisnički definirano polje uspješno dodano';
+$lang['message_udf_successfully_deleted'] = 'Korisnički definirano polje uspješno izbrisano';
+$lang['message_unable_to_determine_root'] = 'Nismo u mogućnosti očitati root korisnički račun. Molimo provjerite svoju konfiguraciju.';
+$lang['message_unable_to_find_file'] = 'Tražena datoteka nije pronađena';
+$lang['message_user_exists'] = 'Korisnik sa tim imenom trenutno postoji. Molimo <a href=\'signup.php\'>pokušajte ponovo</a>';
+$lang['message_user_successfully_added'] = 'Korisnik uspješno dodan';
+$lang['message_user_successfully_deleted'] = 'Korisnik uspješno izbrisan';
+$lang['message_user_successfully_updated'] = 'Korisnik uspješno izmjenjen';
+$lang['message_wrong_file_checkin'] = 'Pogrešna datoteka! Molimo prijavite ispravnu datoteku.';
+$lang['message_you_are_not_administrator'] = 'Niste administrator!';
+$lang['message_you_did_not_enter_value'] = 'Niste unijeli vrijednost!';
+$lang['message_you_do_not_have_an_account'] = 'Trenutno nemate otvoren korisnički račun. Molimo kontaktirajte administratora kako bi ste zatražili korisnički račun.';
+$lang['message_you_must_assign_rights'] = 'Morate dodjeliti dozvolu za gledanje/modificiranje datoteke barem jednom korisniku.';
+$lang['message_your_password_has_been_changed'] = 'Vaša lozinka je promjenjena.';
+
+$lang['view'] = $lang['label_view'];
