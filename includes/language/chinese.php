bd868aa62933f0139cff00269177489b42fd55fd
logart <logart@769e0422-6c0b-0410-966c-d94082ee0ac6>
2011-05-23 19:21:32 +0000

    0000390: Chinese Translation
    git-svn-id: http://opendocman.svn.sourceforge.net/svnroot/opendocman/opendocman/trunk@813 769e0422-6c0b-0410-966c-d94082ee0ac6

---
+++ includes/language/chinese.php
@@ -0,0 +1,327 @@
+<?php
+//CHINESE LANGUAGE FILE
+// (C) 2010-2011 Stephen Lawrence Jr.
+// Translated by:
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+$lang = array();
+global $lang;
+
+//Login
+$lang['anonymous_link'] = 'Click here to view public files anonymously匿名查看公共文件点这里';
+$lang['forgotpassword'] = 'Forgot your password?忘记密码？';
+$lang['install_folder']  = 'Security Notice: If you already installed/updated then you should remove the \'install\' folder before proceeding安全注意：如果你已完成安装或升级请移除install目录';
+$lang['signup'] = 'Sign-up for an account申请账号';
+$lang['welcome'] = 'Welcome to OpenDocMan欢迎进入OpenDocMan';
+$lang['welcome2'] = 'Log in to begin using The system\'s powerful storage, publishing and revision control features.登录进来开始使用本系统强大的文档存储，发布和版本控制功能';
+$lang['welcome_anonymous_title'] = 'OpenDocMan Anonymous Page: List All--OpenDocMan 匿名页：所有列表';
+$lang['welcome_anonymous_h1'] = 'OpenDocMan Anonymous Page--OpenDocMan 匿名页';
+
+//GLOBAL TERMS
+$lang['action'] = 'Action行动';
+$lang['all'] = 'All所有';
+$lang['anonymous'] = 'Anonymous匿名';
+$lang['author'] = 'Author作者';
+$lang['category'] = 'Category分类';
+$lang['choose'] = 'Choose选择';
+$lang['date'] = 'Date日期';
+$lang['days'] = 'days天';
+$lang['department'] = 'Department部门';
+$lang['edit'] = 'Edit编辑';
+$lang['enter'] = 'Enter进入';
+$lang['error'] = 'Error错误';
+$lang['file'] = 'File文档';
+$lang['help'] = 'Help帮助';
+$lang['home'] = 'Home回家';
+$lang['installer'] = 'Installer安装者';
+$lang['list'] = 'List列表';
+$lang['login'] = 'Login登录';
+$lang['logout'] = 'Logout登出';
+$lang['new'] = 'New新';
+$lang['owner'] = 'Owner拥有者';
+$lang['preferences'] = 'Preferences选择';
+$lang['password'] = 'Password密码';
+$lang['please'] = 'Please请';
+$lang['profile'] = 'Profile外形';
+$lang['public'] = 'Public公共';
+$lang['revision'] = 'Revision版本';
+$lang['search'] = 'Search搜索';
+$lang['select'] = 'Select选择';
+$lang['sincerely'] = 'Sincerely真诚的';
+$lang['submit'] = 'Submit提交';
+$lang['time'] = 'Time时间';
+$lang['type'] = 'Type类型';
+$lang['updated'] = 'Updated更新';
+$lang['username'] = 'Username用户名';
+$lang['users'] = 'Users用户';
+$lang['value'] = 'Value值';
+$lang['view'] = 'View查看';
+
+// Areas
+$lang['area_add_new_category'] = 'Add New Category增加新类别';
+$lang['area_add_new_department'] = 'Add New Department增加新部门';
+$lang['area_add_new_file'] = 'Add New File增加新文档';
+$lang['area_add_new_udf'] = 'Add New User Defined Field增加新用户定义域';
+$lang['area_add_new_user'] = 'Add New User增加新用户';
+$lang['area_admin'] = 'Administration管理';
+$lang['area_check_in_file'] = 'Check-in File签入文档';
+$lang['area_check_out_file'] = 'Check-out File签出文档';
+$lang['area_choose_department'] = 'Choose Department选择部门';
+$lang['area_delete_category'] = 'Delete Category删除类别';
+$lang['area_deleted_files'] = 'Deleted Files已删除文档';
+$lang['area_department_information'] = 'Department Information部门信息';
+$lang['area_display_category'] = 'Display All Files In Category按分类显示所有文档';
+$lang['area_document_listing'] = 'Document Listing文档列表';
+$lang['area_file_delete'] = 'Delete File删除文档';
+$lang['area_file_details'] = 'File Details文档详情';
+$lang['area_file_expiration'] = 'File Expiration文档作废';
+$lang['area_personal_profile'] = 'Personal Profile个人情况';
+$lang['area_reset_password'] = 'Reset Password初始化密码';
+$lang['area_update_category'] = 'Update Category变更分类';
+$lang['area_update_department'] = 'Update Department变更部门';
+$lang['area_update_file'] = 'Update File变更文档';
+$lang['area_view_category'] = 'View Category查看分类';
+
+// Buttons
+$lang['button_add_category'] = 'Add Category增加分类';
+$lang['button_add_department'] = 'Add Department增加部门';
+$lang['button_add_document'] = 'Add Document增加文档';
+$lang['button_authorize'] = 'Authorize授权';
+$lang['button_back'] = 'Back退回';
+$lang['button_cancel'] = 'Cancel暂停';
+$lang['button_check_in'] = 'Check-in签入';
+$lang['button_clear_status'] = 'Clear Status清除状态';
+$lang['button_click_here'] = 'Click Here点这里';
+$lang['button_continue'] = 'Continue继续';
+$lang['button_delete'] = 'Delete删除';
+$lang['button_delete_files'] = 'Delete Files删除文档';
+$lang['button_display_category'] = 'Display All Files In This Category显示本分类所有文档';
+$lang['button_modify_category'] = 'Modify Category修改分类';
+$lang['button_modify_department'] = 'Modify Department修改部门';
+$lang['button_reject'] = 'Reject退回';
+$lang['button_reset'] = 'Reset复位';
+$lang['button_resubmit_for_review'] = 'Re-Submit For Review重新提交评审';
+$lang['button_save'] = 'Save保存';
+$lang['button_undelete'] = 'Undelete反删除';
+$lang['button_update'] = 'Update修订';
+$lang['button_view_department'] = 'View Department查看部门';
+$lang['button_yes'] = 'Yes是';
+
+//Email
+$lang['email_added_to_repository'] = 'added to repository增加存储';
+$lang['email_a_new_file_has_been_added'] = 'A new file has been added新增一个文档';
+$lang['email_a_new_file_has_been_rejected'] = 'A new file has been rejected新文档被退回';
+$lang['email_automated_document_messenger'] = 'Automated Document Messenger自动文档来信';
+$lang['email_because_you_did_not_revise'] = 'because you did not revise it for more than因为你未修改已超过';
+$lang['email_comments_regarding_review'] = 'Comments regarding your review of the document对你的文档进行评审的意见';
+$lang['email_email_all_users'] = 'E-mail all users对所有用户发送邮件';
+$lang['email_email_these_users'] = 'E-mail theses users对这些用户发送邮件';
+$lang['email_email_whole_department'] = 'E-mail whole department对所有部门发送邮件';
+$lang['email_file_expired'] = 'A File Has Expired一个文档作废';
+$lang['email_file_was_rejected_because'] = 'Your file was rejected because you did not revise it for more than你的文档被退回因为你没有修改以超过';
+$lang['email_file_was_rejected_expired'] = 'Your file has expired. Please update the file as soon as possible. Your file may be inaccessible until you do.你的文档已作废，请尽快更新文档，你的文档可能在你更新前不能查阅';
+$lang['email_for_the_following_reasons'] = 'for the following reasons因以下原因';
+$lang['email_greeting'] = 'Dear author亲爱的作者';
+$lang['email_i_would_like_to_inform'] = 'I would like to inform you that我很高兴通知你';
+$lang['email_note_to_authors'] = 'Note to Author(s)对作者的注释';
+$lang['email_revision_days'] = 'Maximum number of days before expiration:最大有效天数';
+$lang['email_salute'] = 'Sincerely真诚的';
+$lang['email_someone_has_requested_password'] = 'Someone has requested a password reset.  If you wish to reset your password please follow the link below.  If you do not wish to reset your password then simply do nothing and disregard this email.';
+$lang['email_status_expired'] = 'Status: Expired';
+$lang['email_subject_review_status'] = 'Review status for document ';
+$lang['email_subject'] = 'Subject';
+$lang['email_thank_you'] = 'Thank You';
+$lang['email_to'] = 'To';
+$lang['email_was_declined_for_publishing_at'] = 'Was declined for publishing at';
+$lang['email_was_rejected_from_repository'] = 'Was rejected from repository';
+$lang['email_you_can_now_login'] = 'You can now log into your account at this page';
+$lang['email_your_account_created'] = 'your document management account was created at';
+$lang['email_your_file_has_been_authorized'] = 'Your file has been authorized for publication';
+
+// Labels
+$lang['label_active'] = 'Active';
+$lang['label_add'] = 'Add';
+$lang['label_admin'] = 'Admin';
+$lang['label_allowed'] = 'Allowed';
+$lang['label_all_departments'] = 'All Departments';
+$lang['label_assign_to'] = 'Assign To';
+$lang['label_author'] = 'Author';
+$lang['label_browse_by'] = 'Filter by:';
+$lang['label_case_sensitive'] = 'Case Sensitive';
+$lang['label_checked_out_files'] = 'Checked-Out Files';
+$lang['label_check_expiration'] = 'Check Expiration';
+$lang['label_comment'] = 'Comment';
+$lang['label_created_date'] = 'Date Created';
+$lang['label_default_for_unset'] = 'Default Setting for Unset Department';
+$lang['label_delete'] = 'Delete';
+$lang['label_delete_undelete'] = 'Delete/Undelete';
+$lang['label_department_authority'] = 'Deptartments Authority';
+$lang['label_department'] = 'Department';
+$lang['label_departments'] = 'Departments';
+$lang['label_department_to_modify'] = 'Department to modify';
+$lang['label_description'] = 'Description';
+$lang['label_display'] = 'Display';
+$lang['label_email_address'] = 'E-Mail Address';
+$lang['label_empty'] = 'Empty';
+$lang['label_exact_phrase'] = 'Exact Phrase';
+$lang['label_example'] = 'Example';
+$lang['label_file_archive'] = 'File Archive';
+$lang['label_file_category'] = 'File Category';
+$lang['label_file_listing'] = 'Files List';
+$lang['label_file_location'] = 'File Location';
+$lang['label_file_maintenance'] = 'File Maintenance';
+$lang['label_filename'] = 'Filename';
+$lang['label_filetype'] = 'File Type';
+$lang['label_filetypes'] = 'File Types';
+$lang['label_file_name'] = 'File Name';
+$lang['label_first_name'] = 'First Name';
+$lang['label_forbidden'] = 'Forbidden';
+$lang['label_found_documents'] = 'found document(s)';
+$lang['label_id'] = 'ID';
+$lang['label_is_admin'] = 'Is Admin';
+$lang['label_is_reviewer'] = 'Is Reviewer';
+$lang['label_last_name'] = 'Last Name';
+$lang['label_moderation'] = 'Moderation';
+$lang['label_modified_date'] = 'Modified Date';
+$lang['label_modify'] = 'Modify';
+$lang['label_name'] = 'Name';
+$lang['label_new_password'] = 'New Password';
+$lang['label_next'] = 'Next';
+$lang['label_note_for_revision_log'] = 'Note for revision log';
+$lang['label_page'] = 'Page';
+$lang['label_phone_number'] = 'Phone Number';
+$lang['label_plugins'] = 'Plug-Ins';
+$lang['label_prev'] = 'Previous';
+$lang['label_read'] = 'Read';
+$lang['label_reassign_to'] = 'Re-assign To';
+$lang['label_rejected_files'] = 'Rejected Files';
+$lang['label_rejections'] = 'Rejections';
+$lang['label_reviewer_for'] = 'Dept. reviewer for';
+$lang['label_reviewer'] = 'Reviewer';
+$lang['label_reviews'] = 'Reviews';
+$lang['label_rights'] = 'Rights';
+$lang['label_run_expiration'] = 'Run Expiration Utility';
+$lang['label_search_term'] = 'Search term';
+$lang['label_select_a_department'] = 'Select a department';
+$lang['label_select_departments'] = 'Select departments';
+$lang['label_select_one'] = 'Select one';
+$lang['label_settings'] = 'Settings';
+$lang['label_size'] = 'Size';
+$lang['label_specific_permissions'] = 'Specific User Permissions (use ctrl-click or cmd-click)';
+$lang['label_status'] = 'Status';
+$lang['label_update'] = 'Update';
+$lang['label_user_defined_fields'] = 'User Defined Fields';
+$lang['label_user_defined_field'] = 'User Defined Field';
+$lang['label_users_in_department'] = 'Users in this department';
+$lang['label_user'] = 'User';
+$lang['label_view'] = 'View';
+
+// Messages
+$lang['message_account_created_add_user'] = 'Your account has been created.';
+$lang['message_account_created_password'] = 'Your randomly generated password is';
+$lang['message_account_created'] = 'Your account has been created. Please check your email for login information.';
+$lang['message_action_cancelled'] = 'Action Cancelled';
+$lang['message_all_actions_successfull'] = 'All actions completed successfully';
+$lang['message_an_email_has_been_sent'] = 'An email has been sent to the email address on file with a link that must be followed in order to reset the password.';
+$lang['message_anonymous_view'] = 'You have been switched to anonymous view mode';
+$lang['message_are_you_sure_remove'] = 'Are you sure you want to remove this?';
+$lang['message_authorized'] = 'Authorized';
+$lang['message_category_successfully_added'] = 'Category successfully added';
+$lang['message_category_successfully_deleted'] = 'Category successfully deleted';
+$lang['message_category_successfully_updated'] = 'Category successfully updated';
+$lang['message_click_to_checkout_document'] = 'Click to check out the selected document and begin downloading it to your local workstation';
+$lang['message_config_value_problem'] = 'There is a problem with one of your configuration values. Please check.';
+$lang['message_current'] = 'Current';
+$lang['message_datadir_problem'] = 'There is a problem with your dataDir. Check to make sure it exists and is writeable';
+$lang['message_datadir_problem_exists'] = 'There is a problem with your dataDir setting. It does not appear to exist.';
+$lang['message_datadir_problem_writable'] = 'There is a problem with your dataDir setting. It does not appear to be writeable to the web server.';
+$lang['message_department_successfully_added'] = 'Department successfully added';
+$lang['message_department_successfully_updated'] = 'Department successfully updated';
+$lang['message_directory_creation_failed'] = 'Directory Creation Failed';
+$lang['message_document_added'] = 'Document successfully added';
+$lang['message_document_checked_in'] = 'Document successfully checked in';
+$lang['message_document_checked_out_to_you'] = 'Documents currently checked out by you';
+$lang['message_document_has_been_archived'] = 'Document has been archived';
+$lang['message_documents_expired'] = 'Documents expired';
+$lang['message_documents_rejected'] = 'Documents rejected';
+$lang['message_document_successfully_deleted'] = 'Document successfully deleted';
+$lang['message_documents_waiting'] = 'Documents waiting to be reviewed';
+$lang['message_error_performing_action'] = 'There was an error performing the request action';
+$lang['message_file_authorized'] = 'File authorization completed successfully';
+$lang['message_file_does_not_exist'] = 'File does not exist or there is a problem with the file.';
+$lang['message_file_expired'] = 'File Expired';
+$lang['message_file_rejected'] = 'File rejection completed successfully';
+$lang['message_folder_error_check'] = 'Folder Error. Check Last Message in status bar for details.';
+$lang['message_folder_perms_error'] = 'Folder Permissions Error:';
+$lang['message_for_further_assistance'] = 'for further assistance';
+$lang['message_found_documents'] = 'found document(s)';
+$lang['message_if_you_are_unable_to_view'] = 'If you are not able to do so for some reason, click <input type="submit" name="submit" value="Download"> to download the selected document and begin downloading it to your local workstation for local view.';
+$lang['message_initial_import'] = 'Initial Import';
+$lang['message_last_message'] = 'Last Message';
+$lang['message_latest_version'] = 'Latest version';
+$lang['message_need_one_department'] = 'You need at least one department';
+$lang['message_no_author_comments_available'] = 'No author comments available';
+$lang['message_no_description_available'] = 'No description available';
+$lang['message_no_documents_checked_out'] = 'No documents are currently checked out to you.';
+$lang['message_no_files_found'] = 'No Files Found';
+$lang['message_no_information_available'] = 'No information available';
+$lang['message_non_unique_account'] = 'Non-Unique account';
+$lang['message_non_unique_key'] = 'Non-Unique key in database.';
+$lang['message_nothing_to_do'] = 'Nothing to do';
+$lang['message_not_writeable'] = 'Not Writeable!';
+$lang['message_once_the_document_has_completed'] = 'Once the document has completed downloading, you may';
+$lang['message_original_version'] = 'Original version';
+$lang['message_please_email'] = 'Please email';
+$lang['message_please_upload_valid_doc'] = 'Please upload a valid document';
+$lang['message_record_exists'] = 'Record already exists. Try again with a different value.';
+$lang['message_rejected'] = 'Rejected';
+$lang['message_rejecting_files'] = 'Rejecting files last edited before';
+$lang['message_reviewers_comments_re_rejection'] = 'Reviewers comments regarding rejection';
+$lang['message_session_error'] = 'Session error. Please login again.';
+$lang['message_set_your_new_password'] = 'Set your new password using the form below.';
+$lang['message_sorry_demo_mode'] = 'Sorry, demo mode only, you can not do that!';
+$lang['message_sorry_not_allowed'] = 'Sorry, you are not allowed do to that!';
+$lang['message_that_filetype_not_supported'] = 'That file type is not currently supported.<p>Please upload a document conforming to any of the following file types or add the missing MIMETYPE to ';
+$lang['message_the_code_you_are_using'] = 'The code you are trying to use to reset your password is no longer valid.  Please use this form to reset your password.';
+$lang['message_the_file_is_too_large'] = 'The file is too large. Check your OpenDocMan settings. Maximum size is';
+$lang['message_the_file_is_too_large_php_ini'] = 'The file is too large for your php server. Check your php.ini configuration for max upload/post/memory. Maximum size is currently: ';
+$lang['message_there_was_an_error_loggin_you_in'] = 'There was an error loggin you in. Please try again.';
+$lang['message_there_was_an_error_performing_the_action'] = 'There was an error performing the requested action.';
+$lang['message_the_username_you_entered'] = 'The username you entered was not found in our system.  Contact us if you have forgotten your username.';
+$lang['message_this_file_cannot_be_checked_in'] = 'This file cannot be checked in';
+$lang['message_this_file_cannot_be_uploaded'] = 'This file cannot be uploaded properly';
+$lang['message_this_operation_cannot_be_done_file'] = 'This operation cannot be done on this file';
+$lang['message_this_operation_cannot_be_done_rev'] = 'This operation cannot be done to a revision of a file';
+$lang['message_this_page_requires_root'] = 'This page requires root level permission';
+$lang['message_this_site_has_high_security'] = 'This site has a high level of security and we cannot retrieve your password for you.  You can use this form to reset your password.  Enter your username and we will send an email to the email address on file with a link that you must follow to reset your password.  At that point you may set it to anything you wish.';
+$lang['message_to_view_your_file'] = 'To view your file in a new window';
+$lang['message_udf_cannot_be_blank'] = 'The UDF name cannot be blank';
+$lang['message_udf_successfully_added'] = 'User defined field successfully added';
+$lang['message_udf_successfully_deleted'] = 'User defined field successfully deleted';
+$lang['message_unable_to_determine_root'] = 'Unable to determine the root username. Please check your configuration.';
+$lang['message_unable_to_find_file'] = 'Unable to find the requested file';
+$lang['message_user_exists'] = 'That user already exists. Please <a href=\'signup.php\'>try again</a>';
+$lang['message_user_successfully_added'] = 'User successfully added';
+$lang['message_user_successfully_deleted'] = 'User successfully deleted';
+$lang['message_user_successfully_updated'] = 'User successfully updated';
+$lang['message_wrong_file_checkin'] = 'Wrong file! Please check in the right file.';
+$lang['message_you_are_not_administrator'] = 'You are not an administrator';
+$lang['message_you_did_not_enter_value'] = 'You did not enter a value!';
+$lang['message_you_do_not_have_an_account'] = 'You do not currently have an account. Please contact the administrator to request one.';
+$lang['message_you_must_assign_rights'] = 'You must assign view/modify rights to at least one user.';
+$lang['message_your_password_has_been_changed'] = 'Your password has been changed.';
+
+$lang['view'] = $lang['label_view'];
