e2a6546a171c28d0ec760d07ac2dc9864c95ed61
Stephen Lawrence <steve@opendocman.com>
2012-09-29 13:54:44 -0700

    Removed the old SVN repo from this branch

--- opendocman/branches/rel-1-3/opendocman/install/upgrade_11rc2.php
+++
@@ -1,6 +0,0 @@
-<?php
-// was tinyint(4)
-$result = mysql_query("
-ALTER TABLE data CHANGE COLUMN category category smallint(5) unsigned NOT NULL default '0'
-") or die("<br>Could not update" . mysql_error());
-?>
