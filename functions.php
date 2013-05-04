3bfa8435bcfcc87738084c0b08628f1ac2d33a87
Stephen Lawrence <steve@opendocman.com>
2013-05-04 06:52:52 -0700

    cleanup

--- functions.php
+++ functions.php
@@ -770,35 +770,32 @@ function display_smarty_template($template_file)
     }
 }
 
+/*
+ * callPluginMethod
+ * @param string $method The name of the plugin method being envoked.
+ * @param string $args Any arguments that should be passed to the plugin method
+ * @return null
+ */
 
-    /*
-     * callPluginMethod
-     * @param string $method The name of the plugin method being envoked.
-     * @param string $args Any arguments that should be passed to the plugin method
-     * @return null
-     */
-    function callPluginMethod($method,$args='')
-    {
-        foreach ($GLOBALS['plugin']->pluginslist as $value)
-        {
-            if (!valid_username($value))
-            {
-                echo 'Sorry, your plugin ' . $value . ' is not setup properly';
-            }
-            $plugin_obj = new $value;
-            $plugin_obj->$method($args);
+function callPluginMethod($method, $args = '')
+{
+    foreach ($GLOBALS['plugin']->pluginslist as $value) {
+        if (!valid_username($value)) {
+            echo 'Sorry, your plugin ' . $value . ' is not setup properly';
         }
+        $plugin_obj = new $value;
+        $plugin_obj->$method($args);
     }
+}
 
-    function debug_query($file, $line, $query)
-    {
-        if($GLOBALS['CONFIG']['debug'] == 'True')
-        {
-            $GLOBALS['debug_text'] .= $file . ': Line #' . $line . ": ". $query . '<br />';
-        }
+function debug_query($file, $line, $query)
+{
+    if ($GLOBALS['CONFIG']['debug'] == 'True') {
+        $GLOBALS['debug_text'] .= $file . ': Line #' . $line . ": " . $query . '<br />';
     }
-    
-    function xss_clean($str)
+}
+
+function xss_clean($str)
 {
     // http://svn.bitflux.ch/repos/public/popoon/trunk/classes/externalinput.php
     // +----------------------------------------------------------------------+
@@ -830,49 +827,45 @@ function display_smarty_template($template_file)
     //   * Split up alternation alternatives
     //   * Made some quantifiers possessive
     // * Handle arrays recursively
- 
-    if (is_array($str) OR is_object($str))
-    {
-        foreach ($str as $k => $s)
-        {
+
+    if (is_array($str) OR is_object($str)) {
+        foreach ($str as $k => $s) {
             $str[$k] = xss_clean($s);
         }
- 
+
         return $str;
     }
- 
+
     // Remove all NULL bytes
     $str = str_replace("\0", '', $str);
- 
+
     // Fix &entity\n;
-    $str = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $str);
+    $str = str_replace(array('&amp;', '&lt;', '&gt;'), array('&amp;amp;', '&amp;lt;', '&amp;gt;'), $str);
     $str = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $str);
     $str = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $str);
     $str = html_entity_decode($str, ENT_COMPAT);
- 
+
     // Remove any attribute starting with "on" or xmlns
     $str = preg_replace('#(?:on[a-z]+|xmlns)\s*=\s*[\'"\x00-\x20]?[^\'>"]*[\'"\x00-\x20]?\s?#iu', '', $str);
- 
+
     // Remove javascript: and vbscript: protocols
     $str = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $str);
     $str = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $str);
     $str = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $str);
- 
+
     // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
     $str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#is', '$1>', $str);
     $str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#is', '$1>', $str);
     $str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#ius', '$1>', $str);
- 
+
     // Remove namespaced elements (we do not need them)
     $str = preg_replace('#</*\w+:\w[^>]*+>#i', '', $str);
- 
-    do
-    {
+
+    do {
         // Remove really unwanted tags
         $old = $str;
         $str = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $str);
-    }
-    while ($old !== $str);
- 
+    } while ($old !== $str);
+
     return $str;
 }
