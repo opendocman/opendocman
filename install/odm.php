64c9a9a96716f5d6035379620d5a36f0a4bc1913
Stephen Lawrence <steve@opendocman.com>
2013-06-15 22:08:53 -0700

    added application/zip

--- install/odm.php
+++ install/odm.php
@@ -372,6 +372,7 @@ $sql_operations=array(
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'application/mspowerpoint', 1);",
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'application/octet-stream', 1);",
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'application/x-zip-compressed', 1);",
+"INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'application/zip', 1);",
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'image/tiff', 1);",
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NUll, 'image/tif', 1);",
 "INSERT INTO `{$dbprefix}filetypes` VALUES(NULL, 'application/vnd.ms-powerpoint', 1);",
