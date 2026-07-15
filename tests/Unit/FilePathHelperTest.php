6a821bb8a55c59a9b31f4e3b9debb672b5f2c425
OpenDocMan <226553971+gh-org-bot-odm@users.noreply.github.com>
2026-07-14 20:30:49 -0400

    feat: add getFilePath() helper for original filename storage

---
+++ tests/Unit/FilePathHelperTest.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../bootstrap.php';
+
+class FilePathHelperTest extends TestCase
+{
+    public function testSanitizeStripsPathTraversal()
+    {
+        $this->assertEquals('file.txt', sanitizeFilename('../file.txt'));
+        $this->assertEquals('file.txt', sanitizeFilename('..\\file.txt'));
+    }
+
+    public function testSanitizeStripsNullBytes()
+    {
+        $this->assertEquals('file.txt', sanitizeFilename("file\x00.txt"));
+    }
+
+    public function testSanitizeStripsLeadingSlashes()
+    {
+        $this->assertEquals('etc/passwd', sanitizeFilename('/etc/passwd'));
+    }
+
+    public function testSanitizeFallsBackOnEmpty()
+    {
+        $this->assertEquals('untitled', sanitizeFilename('.'));
+        $this->assertEquals('untitled', sanitizeFilename(''));
+    }
+
+    public function testSanitizePreservesSpacesAndSpecialChars()
+    {
+        $this->assertEquals('my report (v2).docx', sanitizeFilename('my report (v2).docx'));
+    }
+}
