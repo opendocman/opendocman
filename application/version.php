812ad975ed2bbc767ef736092f2f4a06ccb1ac24
OpenDocMan <226553971+gh-org-bot-odm@users.noreply.github.com>
2026-07-18 07:41:39 -0400

    Update version.php

--- application/version.php
+++ application/version.php
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-const ODM_APP_VERSION = '2.1.0';//x-release-please-version
+const ODM_APP_VERSION = '2.2.0';//x-release-please-version
 const ODM_DB_VERSION = '1.4.1';
 
 // version information
