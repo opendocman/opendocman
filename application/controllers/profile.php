c02e2f658fcbabd390c5f20732fc319a3e68d06d
gh-org-bot-odm <226553971+gh-org-bot-odm@users.noreply.github.com>
2026-08-25 08:12:47 -0400

    fix: don't double-encode email token instruction on profile page

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -42,7 +42,7 @@ if ($token !== '') {
     $rotate_csrf = $GLOBALS['csrf']->getTokenForTemplate('/user');
     echo '<div class="alert alert-info mt-3">';
     echo '<strong>' . htmlspecialchars(msg('email_token')) . ':</strong> <code>' . htmlspecialchars($token) . '</code><br>';
-    echo htmlspecialchars(msg('email_token_instruction'));
+    echo msg('email_token_instruction');
     echo '<br><form method="post" action="user" class="d-inline">';
     echo '<input type="hidden" name="item" value="' . (int) $_SESSION['uid'] . '">';
     echo '<button type="submit" name="submit" value="Rotate Mail Token" class="btn btn-warning btn-sm">' . msg('email_token_rotate') . '</button>';
